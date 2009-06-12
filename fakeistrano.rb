6f75afc447810b37c7332c61627bac45689f236d
Ben Hoskings <ben@hoskings.net>
2009-06-12 19:26:05 +1000

    Added rails_gem_version().

--- fakeistrano.rb
+++ fakeistrano.rb
@@ -8,3 +8,7 @@ end
 def dbname
   'testapp'
 end
+
+def rails_gem_version
+  $1 if File.read("#{RAILS_ROOT}/config/environment.rb") =~ /^[^#]*RAILS_GEM_VERSION\s*=\s*['"]([!~<>=]*\s*[\d.]+)['"]/
+end
