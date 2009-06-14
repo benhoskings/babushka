57d59afcda8245913dc5382e56c2f638d8c2480f
Ben Hoskings <ben@hoskings.net>
2009-06-15 00:28:00 +1000

    Added prototype of top-level Babushka class.

--- babushka.rb
+++ babushka.rb
@@ -2,7 +2,46 @@ require 'monkey_patches'
 require 'dep'
 require 'fakeistrano'
 
-Dir.glob('deps/**/*.rb').each {|f| require f }
 
-Dir.chdir RAILS_ROOT
-Dep('migrated db').meet
+# Dir.chdir RAILS_ROOT
+# Dep('migrated db').meet
+
+class Babushka
+
+  def initialize argv
+    @target_machine = argv[0]
+  end
+
+  def setup
+    if @target_machine.blank?
+      log_error "You have to specify the target machine on the commandline, e.g.:\n$ babushka you@target.machine.com"
+    else
+      @setup = true
+    end
+  end
+
+  def load_deps
+    Dir.glob('deps/**/*.rb').each {|f| require f }
+    log "Loaded #{Dep.deps.count} dependencies."
+    @deps_loaded = true
+  end
+
+  def setup?
+    @setup && @deps_loaded
+  end
+
+  def run
+    if setup?
+      log "Running on #{@target_machine}."
+    end
+  end
+
+end
+
+def Babushka argv
+  b = Babushka.new ARGV
+  b.setup && b.load_deps
+  b
+end
+
+Babushka(ARGV).run
