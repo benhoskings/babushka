79182608e4301f0ed5c461a6ff841d6b6ef2ce0a
Ben Hoskings <ben@hoskings.net>
2009-06-19 22:29:12 +1000

    Added macports and apt ext_deps.

---
+++ deps/pkg_managers.rb
@@ -0,0 +1,13 @@
+ext_dep 'macports' do
+  requires 'compiler toolchain'
+  if_missing 'port' do
+    log_and_open "Install the MacPorts release for your system, and then run Babushka again.",
+      "http://www.macports.org/install.php"
+  end
+end
+
+ext_dep 'apt' do
+  if_missing 'apt-get' do
+    log "Your system doesn't seem to have Apt installed. Is it Debian-based?"
+  end
+end
