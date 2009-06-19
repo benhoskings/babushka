00db04128d66e5d7170010918efdb1149c4e35a5
Ben Hoskings <ben@hoskings.net>
2009-06-19 19:32:23 +1000

    Updated logging with new methods and tweaks, refactored indentation, and added log_verbose and debug.

--- utils.rb
+++ utils.rb
@@ -7,14 +7,34 @@ def returning obj, &block
 end
 
 $log_indent = 0
-$debug = true
+Cfg = {}
+
+def indentation
+  ' ' * $log_indent * 2
+end
 
 def log_error message, opts = {}, &block
   log message, opts.merge(:error => true), &block
 end
 
+def log_verbose message, opts = {}, &block
+  if Cfg[:verbose_logging]
+    log message, opts, &block
+  elsif block_given?
+    yield
+  end
+end
+
+def debug message, opts = {}, &block
+  if Cfg[:debug]
+    log message, opts, &block
+  elsif block_given?
+    yield
+  end
+end
+
 def log message, opts = {}, &block
-  print(' ' * $log_indent * 2)
+  print indentation
   if block_given?
     print "#{message} {\n".colorize('grey')
     $log_indent += 1
@@ -27,16 +47,11 @@ def log message, opts = {}, &block
       end
     end
   else
-    if opts[:error]
-      print "#{message.colorize('red')}\n"
-    else
-      print "#{message}\n"
-    end
+    message.gsub! "\n", "\n#{indentation}"
+    message = message.colorize 'red' if opts[:error]
+    message = message.end_with "\n" unless opts[:newline] == false
+    print message
   end
-end
-
-def debug message, opts = {}, &block
-  log message, opts, &block if $debug
 end
 
 def yaml file_name
