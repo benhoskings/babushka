3992a0b5761c189803000b4f0d16c79d989a9bd6
Ben Hoskings <ben@hoskings.net>
2009-06-19 16:15:36 +1000

    Moved output and result storing from ShellResult to Shell, and improved block and result handling.

--- shell_helpers.rb
+++ shell_helpers.rb
@@ -4,25 +4,22 @@ require 'open4'
 require 'utils'
 
 class Shell
+  attr_reader :cmd, :result, :stdout, :stderr
   class ShellResult
-    attr_reader :stdout, :stderr
-    alias_method :to_s, :stdout
-    alias_method :output, :stdout
-    alias_method :error, :stderr
+    attr_reader :shell
 
-    def initialize result, stdout, stderr, &block
-      @result, @stdout, @stderr, @block = result, stdout, stderr, block
+    def initialize shell, &block
+      @shell, @block = shell, block
     end
 
-    def ok?
-      @result
-    end
+    def ok?; shell.ok? end
 
     def render
+      log_error "`#{shell.cmd}` failed with '#{shell.stderr.split("\n", 3)[0..1].join(', ')}'" unless ok?
       if @block.nil?
-        output if ok?
+        shell.stdout if shell.ok?
       else
-        @block.call ok?, stdout, stderr
+        @block.call shell
       end
     end
   end
@@ -31,15 +28,17 @@ class Shell
     @cmd = cmd
   end
 
+  def ok?; result end
+
   def run &block
     debug "$ #{@cmd}".colorize('grey')
-    _stdout, _stderr = nil, nil
-    status = Open4.popen4 @cmd do |pid,stdin,stdout,stderr|
-      _stdout, _stderr = stdout.read, stderr.read
-    end
-    returning ShellResult.new(status.exitstatus == 0, _stdout.chomp, _stderr, &block).render do |result|
-      debug "`#{@cmd}` failed with '#{_stderr.chomp}'", :error => true unless result
-    end
+    @stdout, @stderr = nil, nil
+
+    @result = Open4.popen4 @cmd do |pid,stdin,stdout,stderr|
+      @stdout, @stderr = stdout.read.chomp, stderr.read.chomp
+    end.exitstatus.zero?
+
+    ShellResult.new(self, &block).render
   end
 end
 
@@ -52,8 +51,8 @@ def which cmd_name, &block
 end
 
 def cmd_dir cmd_name
-  which("#{cmd_name}") {|result, output, error|
-    File.dirname output if result
+  which("#{cmd_name}") {|shell|
+    File.dirname shell.stdout if shell.ok?
   }
 end
 
