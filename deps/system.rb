6ba1780ed5e3348b30f175eaf09dc1d554e0dca8
Ben Hoskings <ben@hoskings.net>
2009-06-20 20:13:28 +1000

    Added logging to 'secured ssh logins' / met?.

--- deps/system.rb
+++ deps/system.rb
@@ -8,7 +8,11 @@ end
 
 dep 'secured ssh logins' do
   requires 'sed'
-  met? { failable_shell('ssh nonexistentuser@localhost').stderr['(publickey)'] }
+  met? {
+    returning failable_shell('ssh nonexistentuser@localhost').stderr['(publickey)'] do |result|
+      log_verbose "sshd #{'only ' if result}accepts #{result.scan(/[a-z]+/).to_list} logins.", :as => (result ? :ok : :error)
+    end
+  }
   meet {
     change_with_sed 'PasswordAuthentication',          'yes', 'no', ssh_conf_path(:sshd)
     change_with_sed 'ChallengeResponseAuthentication', 'yes', 'no', ssh_conf_path(:sshd)
