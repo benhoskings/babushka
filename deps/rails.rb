078b82704ebb9245d2219161b74bb777c07297a7
Ben Hoskings <ben@hoskings.net>
2009-06-21 03:44:00 +1000

    Only log about migrations if the latest version was found.

--- deps/rails.rb
+++ deps/rails.rb
@@ -4,12 +4,14 @@ dep 'migrated db' do
     current_version = rake("db:version") {|shell| shell.stdout.val_for('Current version') }
     latest_version = Dir.glob('db/migrate').push('0').sort.last.split('_', 2).first
     returning current_version == latest_version do |result|
-      if latest_version == '0'
-        log_verbose "This app doesn't have any migrations yet."
-      elsif result
-        log_ok "DB is up to date at migration #{current_version}"
-      else
-        log "DB needs migrating from #{current_version} to #{latest_version}"
+      unless current_version.blank?
+        if latest_version == '0'
+          log_verbose "This app doesn't have any migrations yet."
+        elsif result
+          log_ok "DB is up to date at migration #{current_version}"
+        else
+          log "DB needs migrating from #{current_version} to #{latest_version}"
+        end
       end
     end
   }
