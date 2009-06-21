c90a5a51dd52c5d751d413c17848e37e42e36650
Ben Hoskings <ben@hoskings.net>
2009-06-22 01:23:07 +1000

    Redesigned Babushka class, removing some support code and scaffolding.

--- babushka.rb
+++ babushka.rb
@@ -1,7 +1,6 @@
 $: << File.dirname(__FILE__)
 require 'monkey_patches'
 require 'dep'
-require 'fakeistrano'
 
 
 class Babushka
@@ -9,40 +8,35 @@ class Babushka
     @args = argv.dup
   end
 
-  def load_deps
-    Dir.glob('deps/**/*.rb').each {|f| require f }
-    log "Loaded #{Dep.deps.count} dependencies."
-  end
-
-  def setup
-    @targets = extract_args @args
-    # if @targets.empty?
-    #   exit_with "You have to specify the target machine on the commandline, e.g.:\n$ babushka you@target.machine.com"
-    # end
-
-    load_deps
-    @setup = true
-  end
-
   def run
     setup unless @setup
-    # log "Running on #{@targets.to_list}."
-    Dep('system').meet
-    Dep('user setup').meet
-    Dep('migrated db').meet
+    if @tasks.empty?
+      log "Nothing to do."
+    else
+      @tasks.each {|dep_name| Dep(dep_name).meet }
+    end
   end
 
 
   private
 
+  def setup
+    @tasks = extract_args @args
+    load_deps
+    @setup = true
+  end
+
+  def load_deps
+    Dir.glob('deps/**/*.rb').each {|f| require f }
+    log "Loaded #{Dep.deps.count} dependencies."
+  end
+
   def extract_args args
     Cfg[:verbose_logging] = %w[-q --quiet].map {|arg| args.delete arg }.first.blank?
     Cfg[:debug] = args.delete('--debug')
     Cfg[:dry_run] = !%w[-n --dry-run].map {|arg| args.delete arg }.first.blank?
-
     args
   end
-
 end
 
 def Babushka argv
--- fakeistrano.rb
+++
@@ -1,3 +0,0 @@
-def rails_gem_version
-  $1 if File.read("#{RAILS_ROOT}/config/environment.rb") =~ /^[^#]*RAILS_GEM_VERSION\s*=\s*['"]([!~<>=]*\s*[\d.]+)['"]/
-end
