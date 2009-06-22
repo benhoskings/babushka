a9b375ea529be2b8a353ff4a89942f7117fe4e2f
Ben Hoskings <ben@hoskings.net>
2009-06-23 01:42:22 +1000

    Updated DepDefiner#cmds_in_path to distinguish between missing and incorrectly located commands.

--- dep_definer.rb
+++ dep_definer.rb
@@ -103,18 +103,18 @@ class PkgDepDefiner < DepDefiner
   end
 
   def cmds_in_path
-    good, bad = provides_or_default.partition {|cmd_name|
-      pkg_manager.cmd_in_path? cmd_name
-    }
+    present, missing = provides_or_default.partition {|cmd_name| cmd_dir(cmd_name) }
+    good, bad = present.partition {|cmd_name| pkg_manager.cmd_in_path? cmd_name }
 
     log_ok "#{good.map {|i| "'#{i}'" }.to_list} run#{'s' if good.length == 1} from #{cmd_dir(good.first)}." unless good.empty?
+    log_error "#{missing.map {|i| "'#{i}'" }.to_list} #{missing.length == 1 ? 'is' : 'are'} missing from your PATH." unless missing.empty?
 
     unless bad.empty?
       log_error "#{bad.map {|i| "'#{i}'" }.to_list} incorrectly run#{'s' if bad.length == 1} from #{cmd_dir(bad.first)}."
       log "You need to put #{pkg_manager.prefix} before #{cmd_dir(bad.first)} in your PATH."
     end
 
-    bad.empty?
+    missing.empty? and bad.empty?
   end
 
   def install_packages
