d3e0e2058754ed1c30b613f01301ad8cb54f1268
Ben Hoskings <ben@hoskings.net>
2009-06-23 02:05:06 +1000

    Updtaed 'www user and group' / meet shells to sudos.

--- deps/web.rb
+++ deps/web.rb
@@ -2,7 +2,7 @@ dep 'www user and group' do
   www_name = osx? ? '_www' : 'www'
   met? { grep(/^#{www_name}\:/, '/etc/passwd') and grep(/^#{www_name}\:/, '/etc/group') }
   meet {
-    shell "groupadd #{www_name}"
-    shell "useradd -g #{www_name} #{www_name} -s /bin/false"
+    sudo "groupadd #{www_name}"
+    sudo "useradd -g #{www_name} #{www_name} -s /bin/false"
   }
 end
