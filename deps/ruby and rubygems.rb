7ddd00f89d5aeed14eaf5cd919b68fd000c3f6e0
Ben Hoskings <ben@hoskings.net>
2009-06-26 02:22:54 +1000

    Removed 'wget ' typo in rubygems dep.

--- deps/ruby and rubygems.rb
+++ deps/ruby and rubygems.rb
@@ -7,7 +7,7 @@ dep 'rubygems' do
     in_dir "~/src" do
       # disable ri and rdoc generation
       shell "sed -i 's/# gem\: --no-rdoc --no-ri/gem\: --no-rdoc --no-ri/' ~/.dot-files/.gemrc"
-      get_source("wget http://rubyforge.org/frs/download.php/57643/rubygems-#{rubygems_version}.tgz") and
+      get_source("http://rubyforge.org/frs/download.php/57643/rubygems-#{rubygems_version}.tgz") and
 
       in_dir "rubygems-#{rubygems_version}" do
         sudo "ruby setup.rb"
