50720469f3802813809d49cd70d466584f55b2a9
Ben Hoskings <ben@hoskings.net>
2009-07-06 02:32:47 +1000

    Moved String#starts_with? and related methods to new StartsAndEndsChecks module, and included it in both Array and String.

--- lib/support/core_patches.rb
+++ lib/support/core_patches.rb
@@ -1,4 +1,28 @@
+module StartsAndEndsChecks
+  # Returns true iff +str+ appears exactly at the start of +self+.
+  def starts_with? str
+    self[0, str.length] == str
+  end
+
+  # Returns true iff +str+ appears exactly at the end of +self+.
+  def ends_with? str
+    self[-str.length, str.length] == str
+  end
+
+  # Return a duplicate of +self+, with +str+ prepended to it if it doesn't already start with +str+.
+  def start_with str
+    starts_with?(str) ? self : str + self
+  end
+
+  # Return a duplicate of +self+, with +str+ appended to it if it doesn't already end with +str+.
+  def end_with str
+    ends_with?(str) ? self : self + str
+  end
+end
+
 class Array
+  include StartsAndEndsChecks
+
   def cut &block
     if (cut_at = index {|i| yield i }).nil?
       [self, nil]
@@ -85,25 +109,7 @@ class Object
 end
 
 class String
-  # Returns true iff +str+ appears exactly at the start of +self+.
-  def starts_with? str
-    self[0, str.length] == str
-  end
-
-  # Returns true iff +str+ appears exactly at the end of +self+.
-  def ends_with? str
-    self[-str.length, str.length] == str
-  end
-
-  # Return a duplicate of +self+, with +str+ prepended to it if it doesn't already start with +str+.
-  def start_with str
-    starts_with?(str) ? self : str + self
-  end
-
-  # Return a duplicate of +self+, with +str+ appended to it if it doesn't already end with +str+.
-  def end_with str
-    ends_with?(str) ? self : self + str
-  end
+  include StartsAndEndsChecks
 
   def val_for key
     split("\n").grep(/\b#{key}\b/).map {|l| l.sub(/\b#{key}\b\:?/, '').sub(/;\s*$/, '').strip }.first || ''
