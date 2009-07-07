7afb673cf43a39bec4826708bb9d9435854571a3
Ben Hoskings <ben@hoskings.net>
2009-07-08 01:57:37 +1000

    Refactored yield_counts into helper, hashing them by dep name.

--- spec/dep_helper.rb
+++ spec/dep_helper.rb
@@ -1,9 +1,21 @@
+def setup_yield_counts
+  @yield_counts = Hash.new {|hsh,k| hsh[k] = Hash.new {|hsh,k| 0 } }
+
+  @yield_counts_none = {}
+  @yield_counts_met_run = {:setup => 1, :met? => 1}
+  @yield_counts_meet_run = {:setup => 1, :met? => 2, :meet => 1, :before => 1, :after => 1}
+  @yield_counts_dep_failed = {:setup => 1}
+  @yield_counts_failed_meet_run = {:setup => 1, :met? => 2, :meet => 1, :before => 1, :after => 1}
+  @yield_counts_already_met = {:setup => 1, :met? => 1}
+  @yield_counts_failed_at_before = {:setup => 1, :met? => 2, :before => 1}
+end
+
 def make_counter_dep opts = {}
   incrementers = DepDefiner.accepted_blocks.inject({}) {|lambdas,key|
-    lambdas[key] = L{ @yield_counts[key] += 1 }
+    lambdas[key] = L{ @yield_counts[opts[:name]][key] += 1 }
     lambdas
   }
-  dep [opts[:name], 'lambda counter'].compact.join(' ') do
+  dep opts[:name] do
     DepDefiner.accepted_blocks.each {|dep_method|
       send dep_method, &L{
         returning (opts[dep_method] || @dep.send(:default_task, dep_method)).call do
--- spec/dep_spec.rb
+++ spec/dep_spec.rb
@@ -18,62 +18,61 @@ describe "dep creation" do
       end
     }.should change(Dep, :count).by(1)
   end
+  after { Dep.clear! }
 end
 
 describe "calling met? on a single dep" do
-  before do
-    @yield_counts = Hash.new {|hsh,k| 0 }
-    @yield_counts_met_run = {:setup => 1, :met? => 1}
-  end
+  before {
+    setup_yield_counts
+  }
   it "should return false for unmet deps" do
     make_counter_dep(
       :name => 'unmeetable for met', :met? => L{ false }
     ).met?.should == false
-    @yield_counts.should == @yield_counts_met_run
+    @yield_counts['unmeetable for met'].should == @yield_counts_met_run
   end
   it "should return true for already met deps" do
     make_counter_dep(
       :name => 'met for met'
     ).met?.should == true
-    @yield_counts.should == @yield_counts_met_run
+    @yield_counts['met for met'].should == @yield_counts_met_run
   end
+  after { Dep.clear! }
 end
 
-describe "calling met? on a single dep" do
-  before do
-    @yield_counts = Hash.new {|hsh,k| 0 }
-    @yield_counts_already_met = {:setup => 1, :met? => 1}
-    @yield_counts_meet_run = {:setup => 1, :met? => 2, :meet => 1, :before => 1, :after => 1}
-    @yield_counts_failed_at_before = {:setup => 1, :met? => 2, :before => 1}
-  end
+describe "calling meet on a single dep" do
+  before {
+    setup_yield_counts
+  }
   it "should fail twice on unmeetable deps" do
     make_counter_dep(
       :name => 'unmeetable', :met? => L{ false }
     ).meet.should == false
-    @yield_counts.should == @yield_counts_meet_run
+    @yield_counts['unmeetable'].should == @yield_counts_meet_run
   end
   it "should fail, run meet, and then succeed on unmet deps" do
     make_counter_dep(
-      :name => 'unmet', :met? => L{ !@yield_counts[:met?].zero? }
+      :name => 'unmet', :met? => L{ !@yield_counts['unmet'][:met?].zero? }
     ).meet.should == true
-    @yield_counts.should == @yield_counts_meet_run
+    @yield_counts['unmet'].should == @yield_counts_meet_run
   end
   it "should fail, not run meet, and fail again on unmet deps where before fails" do
     make_counter_dep(
       :name => 'unmet, #before fails', :met? => L{ false }, :before => L{ false }
     ).meet.should == false
-    @yield_counts.should == @yield_counts_failed_at_before
+    @yield_counts['unmet, #before fails'].should == @yield_counts_failed_at_before
   end
   it "should fail, run meet, and then succeed on unmet deps where after fails" do
     make_counter_dep(
-      :name => 'unmet, #after fails', :met? => L{ !@yield_counts[:met?].zero? }, :after => L{ false }
+      :name => 'unmet, #after fails', :met? => L{ !@yield_counts['unmet, #after fails'][:met?].zero? }, :after => L{ false }
     ).meet.should == true
-    @yield_counts.should == @yield_counts_meet_run
+    @yield_counts['unmet, #after fails'].should == @yield_counts_meet_run
   end
   it "should succeed on already met deps" do
     make_counter_dep(
       :name => 'met', :met? => L{ true }
     ).meet.should == true
-    @yield_counts.should == @yield_counts_already_met
+    @yield_counts['met'].should == @yield_counts_already_met
   end
+  after { Dep.clear! }
 end
