0671edf5d92c8853de26e567d2fee3994fbc85e1
Ben Hoskings <ben@hoskings.net>
2009-07-08 00:38:53 +1000

    Cleaned up deps/packages.rb.

--- deps/packages.rb
+++ deps/packages.rb
@@ -1,45 +1,33 @@
-
-pkg 'git' do
-  installs 'git-core'
-end
-
-pkg 'fish'
-
-pkg 'rcconf' do
-  installs :apt => 'rcconf'
-end
-
-pkg 'sed' do
-  installs :macports => 'gsed'
-  provides 'gsed'
-end
-
-pkg 'wget'
-
+pkg 'autoconf'
 pkg 'build-essential' do
   provides 'gcc', 'g++', 'make', 'ld'
 end
-
-pkg 'autoconf'
-
-gem 'passenger' do
-  provides 'passenger-install-nginx-module'
+pkg 'fish'
+pkg 'git' do
+  installs 'git-core'
 end
-
-pkg 'vim'
-
+pkg 'java' do
+  installs :apt => 'sun-java6-jre'
+  provides 'java'
+  after { shell "set -Ux JAVA_HOME /usr/lib/jvm/java-6-sun" }
+end
 pkg 'libssl headers' do
   installs :apt => 'libssl-dev'
   provides []
 end
-
+gem 'passenger' do
+  provides 'passenger-install-nginx-module'
+end
+pkg 'rcconf' do
+  installs :apt => 'rcconf'
+end
+pkg 'sed' do
+  installs :macports => 'gsed'
+  provides 'gsed'
+end
+pkg 'vim'
+pkg 'wget'
 pkg 'zlib headers' do
   installs :apt => 'zlib1g-dev'
   provides []
-end
-
-pkg 'java' do
-  installs :apt => 'sun-java6-jre'
-  provides 'java'
-  after { shell("set -Ux JAVA_HOME /usr/lib/jvm/java-6-sun") }
 end
