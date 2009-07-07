39126249322d2ea7550e91df176dc64acf3ce3d6
Ben Hoskings <ben@hoskings.net>
2009-07-08 01:56:39 +1000

    Disabled logging during spec runs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,12 @@ include Babushka::ShellHelpers
 def tmp_prefix
   "#{'/private' if osx?}/tmp/rspec/its_ok_if_a_test_deletes_this"
 end
+
+module Babushka
+  class Logger
+    def self.log message, opts = {}, &block
+      # Don't log while running specs.
+      yield if block_given?
+    end
+  end
+end
