f069315fd46780f39456fd7bd265cd3b3a5a7a25
Ben Hoskings <ben@hoskings.net>
2009-07-09 00:19:41 +1000

    Added 'ruby 1.9' dep.

--- deps/ruby and rubygems.rb
+++ deps/ruby and rubygems.rb
@@ -24,3 +24,8 @@ pkg 'ruby' do
   installs :macports => 'ruby', :apt => %w[ruby irb ri rdoc ruby1.8-dev libopenssl-ruby]
   provides %w[ruby irb ri rdoc]
 end
+
+pkg 'ruby 1.9' do
+  installs :macports => 'ruby19', :apt => %w[ruby1.9 irb1.9 ri1.9 rdoc1.9 ruby1.9-dev libopenssl-ruby1.9]
+  provides %w[ruby1.9 irb1.9 ri1.9 rdoc1.9]
+end
