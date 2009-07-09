19bc743e2468bdd8171fab6f3fed7a2e56c51998
Ben Hoskings <ben@hoskings.net>
2009-07-10 04:51:38 +1000

    Added GemDepDefiner specs.

---
+++ spec/gem_dep_definer_spec.rb
@@ -0,0 +1,21 @@
+require 'spec/spec_support'
+require 'spec/dep_definer_support'
+require 'spec/pkg_dep_definer_support'
+require 'spec/gem_dep_definer_support'
+
+describe "accepts_hash_for default values" do
+  before {
+    make_test_pkgs :gem
+    make_test_gems
+  }
+
+  it "should handle versions" do
+    Dep('default gem').definer.installs.should == ['default gem']
+    Dep('single gem with version').definer.installs.should == {'gem1' => '1.2.3'}
+    Dep('multiple gems with version').definer.installs.should == {'gem2' => '0.1.4', 'gem3' => '0.2.5.1'}
+  end
+
+  after {
+    Dep.clear!
+  }
+end
