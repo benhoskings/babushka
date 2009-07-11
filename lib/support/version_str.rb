97851350689fc99d2dee05f4982e7ce1ddcdeb2b
Ben Hoskings <ben@hoskings.net>
2009-07-11 20:18:54 +1000

    Added operator support to VersionStr.

--- lib/support/version_str.rb
+++ lib/support/version_str.rb
@@ -2,14 +2,18 @@ module Babushka
   class VersionStr
 
     include Comparable
-
-    attr_reader :pieces
+    attr_reader :pieces, :operator
+    GemVersionOperators = %w[= != > < >= <= ~>].freeze
 
     def <=> other
       pieces <=> other.pieces
     end
     def initialize str
-      @pieces = str.split('.').map(&:to_i)
+      captures = str.scan(/^(#{GemVersionOperators.join('|')})?\s*([0-9.]+)$/)
+      unless captures.nil? || captures.first.nil? || captures.first.last.nil?
+        @pieces = captures.first.last.split('.').map(&:to_i)
+        @operator = captures.first.first
+      end
     end
     def to_s
       pieces.join('.')
