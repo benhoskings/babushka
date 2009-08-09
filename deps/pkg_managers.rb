0feb1527f61591727208bd7b97163b1cd9cca595
Ben Hoskings <ben@hoskings.net>
2009-08-10 00:07:35 +1000

    The 'macports' dep can install macports now, instead of just loading the instructions.

--- deps/pkg_managers.rb
+++ deps/pkg_managers.rb
@@ -1,9 +1,16 @@
-ext 'macports' do
+dep 'macports' do
   requires 'build tools'
-  if_missing 'port' do
-    log_and_open "Install the MacPorts release for your system, and then run Babushka again.",
-      "http://www.macports.org/install.php"
-  end
+  run_as 'root'
+  merge :versions, :macports => '1.7.1'
+  met? { which 'port' }
+  meet {
+    in_build_dir {
+      dmg "http://svn.macports.org/repository/macports/downloads/MacPorts-#{versions[:macports]}/MacPorts-#{versions[:macports]}-#{system_release}-#{system_name}.dmg" do |path|
+        log_shell "Installing MacPorts-#{versions[:macports]}", "installer -pkg #{path / "MacPorts-#{versions[:macports]}.pkg"} -target /", :sudo => true
+      end
+    }
+  }
+  after { log_shell "Running port selfupdate", "port selfupdate", :sudo => true }
 end
 
 ext 'apt' do
