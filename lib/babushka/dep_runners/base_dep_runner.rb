413a184779dbe9ab34fc864b76090ff3d7d553a2
Ben Hoskings <ben@hoskings.net>
2009-08-10 00:07:18 +1000

    Added BaseDepRunner#dmg.

--- lib/babushka/dep_runners/base_dep_runner.rb
+++ lib/babushka/dep_runners/base_dep_runner.rb
@@ -1,5 +1,18 @@
 module Babushka
   class BaseDepRunner < DepRunner
 
+    private
+
+    def dmg url, &block
+      download url
+      output = shell "hdiutil attach #{File.basename url}"
+      unless output.nil?
+        path = output.val_for(/\/dev\/disk\d+s1\s+Apple_HFS/)
+        returning yield path do
+          shell "hdiutil detach #{path}"
+        end
+      end
+    end
+
   end
 end
