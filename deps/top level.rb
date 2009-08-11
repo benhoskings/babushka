67d40746d3e5b60a19e53c424690286e7ac718c7
Ben Hoskings <ben@hoskings.net>
2009-08-12 02:39:26 +1000

    Remove #run_as and #asks_for calls from top level deps, replacing where appropriate with #define_var.

--- deps/top level.rb
+++ deps/top level.rb
@@ -1,15 +1,15 @@
 dep 'system' do
   requires 'hostname', 'correct path', 'secured ssh logins', 'lax host key checking', 'admins can sudo'
-  run_as 'root'
 end
 
 dep 'user setup' do
   requires 'user shell setup', 'passwordless ssh logins', 'public key', 'vim'
-  run_as :username
+  define_var :username, :default => shell('whoami')
 end
 
 dep 'rails app' do
   requires 'gems installed', 'vhost enabled', 'webserver running', 'migrated db'
-  run_as :username
-  asks_for :domain, :rails_env
+  define_var :domain, :default => :username
+  define_var :rails_env, :default => 'production'
+  define_var :rails_root, :default => '~/current'
 end
