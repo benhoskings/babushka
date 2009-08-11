65af62434de1db43db7adb1b7776ff875f2e3da4
Ben Hoskings <ben@hoskings.net>
2009-08-12 02:19:40 +1000

    Return :fail on missing Xcode, so a meet isn't attempted.

--- deps/os_x.rb
+++ deps/os_x.rb
@@ -2,5 +2,6 @@ ext 'xcode tools' do
   if_missing 'gcc', 'g++', 'autoconf', 'make', 'ld' do
     log_and_open "Install Xcode, and then run Babushka again.",
       "http://developer.apple.com/technology/xcode.html"
+    :fail
   end
 end
