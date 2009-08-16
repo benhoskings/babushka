11326a481d57befd00c68012bc8302cef5d337ec
Ben Hoskings <ben@hoskings.net>
2009-08-16 19:27:13 +1000

    Removed unused #from_first_and_rest method.

--- lib/support/utils.rb
+++ lib/support/utils.rb
@@ -34,10 +34,6 @@ end
 def linux?; :linux == uname end
 def osx?; :osx == uname end
 
-def from_first_and_rest first, rest
-  first.is_a?(Hash) ? first : [*first].concat(rest)
-end
-
 require 'etc'
 def pathify str
   File.expand_path str.sub(/^\~\/|^\~$/) {|_| Etc.getpwuid(Process.euid).dir.end_with('/') }
