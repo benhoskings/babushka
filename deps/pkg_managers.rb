1465cc5374d072867f27a17bc5523522ad9e736b
Ben Hoskings <ben@hoskings.net>
2009-09-01 01:01:41 +1000

    Updated the 'macports' dep to build from source (also bumped the macports version 1.7.1 -> 1.8.0).

--- deps/pkg_managers.rb
+++ deps/pkg_managers.rb
@@ -1,14 +1,8 @@
-dep 'macports' do
+src 'macports' do
   requires 'build tools'
-  merge :versions, :macports => '1.7.1'
-  met? { which 'port' }
-  meet {
-    in_build_dir {
-      dmg "http://svn.macports.org/repository/macports/downloads/MacPorts-#{versions[:macports]}/MacPorts-#{versions[:macports]}-#{system_release}-#{system_name}.dmg" do |path|
-        log_shell "Installing MacPorts-#{versions[:macports]}", "installer -pkg #{path / "MacPorts-#{versions[:macports]}.pkg"} -target /", :sudo => true
-      end
-    }
-  }
+  provides 'port'
+  prefix '/opt/local'
+  source "http://distfiles.macports.org/MacPorts/MacPorts-1.8.0.tar.gz"
   after { log_shell "Running port selfupdate", "port selfupdate", :sudo => true }
 end
 
