b6640b9b17f7427849b0b6a2a904946e4d3a0448
Ben Hoskings <ben@hoskings.net>
2009-08-31 19:02:44 +1000

    Don't require 'db backups' for 'existing db'.

--- deps/rails.rb
+++ deps/rails.rb
@@ -66,7 +66,7 @@ dep 'deployed app' do
 end
 
 dep 'existing db' do
-  requires 'db gem', 'db access', 'db backups', 'rails'
+  requires 'db gem', 'db access', 'rails'
   met? {
     !shell("psql -l") {|shell|
       shell.stdout.split("\n").grep(/^\s*#{var :db_name, :default => :username}\s+\|/)
