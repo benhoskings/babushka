bdcf2aa2ee9dee12022ffe33556234f707a0553c
Ben Hoskings <ben@hoskings.net>
2009-09-16 14:16:00 +1000

    Add opts[:optional] to #render_erb helper.

--- lib/support/shell_helpers.rb
+++ lib/support/shell_helpers.rb
@@ -197,14 +197,19 @@ def babushka_config? path
 end
 
 def render_erb erb, opts = {}
-  require 'erb'
-  debug ERB.new(IO.read(File.dirname(source_path) / erb)).result(binding)
-  returning sudo "cat > #{opts[:to]}", :input => ERB.new(IO.read(File.dirname(source_path) / erb)).result(binding) do |result|
-    if result
-      log "Rendered #{opts[:to]}."
-      sudo "chmod #{opts[:perms]} '#{opts[:to]}'" unless opts[:perms].nil?
-    else
-      log_error "Couldn't render #{opts[:to]}."
+  path = File.dirname(source_path) / erb
+  if !File.exists?(path) && !opts[:optional]
+    log_error "Couldn't find erb to render at #{path}."
+  elsif File.exists?(path)
+    require 'erb'
+    debug ERB.new(IO.read(path)).result(binding)
+    returning sudo "cat > #{opts[:to]}", :input => ERB.new(IO.read(path)).result(binding) do |result|
+      if result
+        log "Rendered #{opts[:to]}."
+        sudo "chmod #{opts[:perms]} '#{opts[:to]}'" unless opts[:perms].nil?
+      else
+        log_error "Couldn't render #{opts[:to]}."
+      end
     end
   end
 end
