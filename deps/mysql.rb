0cd652932ae8467ef8640ae0e278269252622ce6
Ben Hoskings <ben@hoskings.net>
2009-09-18 11:29:59 +1000

    Fixed user handling in 'mysql access', and set a root password with 'mysql root password'.

--- deps/mysql.rb
+++ deps/mysql.rb
@@ -8,14 +8,24 @@ dep 'mysql access' do
   requires 'mysql db exists', 'user exists'
   define_var :db_user, :default => :username
   define_var :db_host, :default => 'localhost'
-  met? { mysql "use #{var(:db_name)}" }
+  met? { mysql "use #{var(:db_name)}", var(:db_user) }
   meet { mysql %Q{GRANT ALL PRIVILEGES ON #{var :db_name}.* TO '#{var :db_user}'@'#{var :db_host}' IDENTIFIED BY '#{var :db_password}'} }
 end
 
 dep 'mysql db exists' do
-  requires 'mysql software'
+  requires 'mysql configured'
   met? { mysql("SHOW DATABASES").split("\n")[3..-1].any? {|l| /\b#{var :db_name}\b/ =~ l } }
   meet { mysql "CREATE DATABASE #{var :db_name}" }
+end
+
+dep 'mysql configured' do
+  requires 'mysql root password'
+end
+
+dep 'mysql root password' do
+  requires 'mysql software'
+  met? { failable_shell('mysql -u root').stderr["Access denied for user 'root'@'localhost' (using password: NO)"] }
+  meet { mysql(%Q{GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '#{var :db_admin_password}'}, 'root', false) }
 end
 
 pkg 'mysql software' do
