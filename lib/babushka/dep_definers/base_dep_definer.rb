49ef741425cfe7a101abfb32cbc47fdff6395cfe
Ben Hoskings <ben@hoskings.net>
2009-09-23 20:15:45 +1000

    Added internal_setup block acceptor and call to all deps.

--- lib/babushka/dep_definers/base_dep_definer.rb
+++ lib/babushka/dep_definers/base_dep_definer.rb
@@ -2,6 +2,7 @@ module Babushka
   class BaseDepDefiner < DepDefiner
 
     accepts_list_for :requires
+    accepts_block_for :internal_setup
     accepts_block_for :setup
     accepts_block_for :met?
     accepts_block_for :meet
