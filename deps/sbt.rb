4e4674f9782cd2fba3215428ffdb28dc80a5004a
Ben Hoskings <ben@hoskings.net>
2009-09-23 23:51:54 +1000

    Updated sbt dep - put the jar in prefix/lib/sbt.

--- deps/sbt.rb
+++ deps/sbt.rb
@@ -1,11 +1,17 @@
 dep 'sbt' do
   requires 'java'
+  merge :versions, :sbt => '0.5.4'
   met? { which 'sbt' }
   meet {
-    in_dir var(:install_path, :default => '/usr/local/bin') do
-      download 'http://simple-build-tool.googlecode.com/files/sbt-launcher-0.5.4.jar', 'sbt-launcher.jar'
-      shell %q{echo 'java -Xmx512M -jar `dirname $0`/sbt-launcher.jar "$@"' > sbt}
-      shell 'chmod +x sbt'
+    in_dir var(:install_path, :default => '/usr/local') do
+      in_dir 'lib/sbt', :create => true do
+        download "http://simple-build-tool.googlecode.com/files/sbt-launcher-#{var(:versions)[:sbt]}.jar"
+        shell "ln -sf sbt-launcher-#{var(:versions)[:sbt]}.jar sbt-launcher.jar"
+      end
+      in_dir 'bin' do
+        shell %Q{echo '#!/bin/bash\njava -Xmx512M -jar `dirname $0`/../lib/sbt/sbt-launcher.jar "$@"' > sbt}
+        shell 'chmod +x sbt'
+      end
     end
   }
 end
