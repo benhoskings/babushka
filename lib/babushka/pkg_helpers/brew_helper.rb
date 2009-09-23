b2c339f8b88612840ff57e3b2ff7127481be286c
Ben Hoskings <ben@hoskings.net>
2009-09-23 17:19:08 +1000

    Added BrewHelper#install! to check formula existence and parse the build result from `brew` output, since the return value can't be used.

--- lib/babushka/pkg_helpers/brew_helper.rb
+++ lib/babushka/pkg_helpers/brew_helper.rb
@@ -7,17 +7,37 @@ module Babushka
     def manager_dep; 'homebrew' end
 
     def install! pkgs
-      pkgs.all? {|pkg|
-        log_shell "Installing #{pkg} via #{manager_key}",
-          "#{pkg_cmd} install #{cmdline_spec_for pkg}",
-          :sudo => should_sudo
+      install_packages! pkgs if pkgs.all? {|pkg|
+        returning has_formula_for? pkg do |result|
+          log_error "There is no formula for '#{pkg}' in #{formulas_prefix}." unless result
+        end
       }
     end
 
     private
 
+    def install_packages! pkgs
+      pkgs.all? {|pkg|
+        log_shell_with_a_block_to_scan_stdout_for_apps_that_have_broken_return_values(
+          "Installing #{pkg} via #{manager_key}",
+          "#{pkg_cmd} install #{cmdline_spec_for pkg}",
+          :sudo => should_sudo
+        ) {|shell|
+          shell.result && shell.stdout["\033[1;31m==>\033[0;0;1m Error\033[0;0m:"].nil?
+        }
+      }
+    end
+
     def _has? pkg
       versions_of(pkg).sort.reverse.detect {|version| pkg.matches? version }
+    end
+
+    def has_formula_for? pkg
+      pkg.name.in? existing_formulas
+    end
+
+    def existing_formulas
+      Dir[formulas_prefix / '*.rb'].map {|i| File.basename i, '.rb' }
     end
 
     def versions_of pkg
@@ -31,6 +51,10 @@ module Babushka
 
     def installed_pkgs_prefix
       prefix / 'Cellar'
+    end
+
+    def formulas_prefix
+      prefix / 'Library/Formula'
     end
 
     def should_sudo
