540e56808a345658d5b57bf2f63b409a1760a7d0
Ben Hoskings <ben@hoskings.net>
2009-09-30 17:49:24 +1000

    Replaced 'ruby' pkg with a chooser and separate deps for the different ruby install methods.

--- deps/ruby and rubygems.rb
+++ deps/ruby and rubygems.rb
@@ -28,7 +28,25 @@ dep 'rubygems installed' do
   }
 end
 
-pkg 'ruby' do
+dep 'ruby' do
+  setup {
+    rubies = {
+      'pkg' => "Install via #{Babushka::PkgHelper.for_system.manager_dep}",
+      'ree' => "Build Ruby Enterprise Edition from source"
+    }
+    rubies['system'] = "Use the OS X-supplied version" if osx?
+    rubies.each_pair {|ruby_type,description|
+      log "#{ruby_type.ljust(rubies.keys.map(&:length).max)} - #{description}"
+    }
+    chosen_ruby = var(:ruby_type,
+      :message => "Which ruby would you like to use",
+      :default => 'pkg'
+    )
+    requires "#{chosen_ruby} ruby"
+  }
+end
+
+pkg 'pkg ruby' do
   installs {
     macports 'ruby'
     apt %w[ruby irb ri rdoc ruby1.8-dev libopenssl-ruby]
@@ -36,6 +54,20 @@ pkg 'ruby' do
   provides %w[ruby irb ri rdoc]
 end
 
+dep 'system ruby', :for => :osx do
+  met? {
+    cmds_in_path? ['ruby', 'irb', 'ri', 'rdoc']
+  }
+end
+
+src 'ree ruby' do
+  source "http://rubyforge.org/frs/download.php/64475/ruby-enterprise-1.8.7-20090928.tar.gz"
+  provides 'ruby', 'irb', 'ri', 'rdoc'
+  met? {
+    log_error "Not implemented yet - bug me on twitter (@ben_h) or even better, send me your dep :)"
+    :fail
+  }
+end
 
 pkg 'ruby 1.9' do
   installs {
