65738f486a233f748a7b57b9a141086af9ea1c23
Ben Hoskings <ben@hoskings.net>
2009-09-30 17:15:04 +1000

    Moved PkgDepRunner#cmds_in_path? to BaseDepRunner, along with pkg_manager delegation.

--- lib/babushka/dep_runners/base_dep_runner.rb
+++ lib/babushka/dep_runners/base_dep_runner.rb
@@ -1,7 +1,36 @@
 module Babushka
   class BaseDepRunner < DepRunner
 
+    delegate :pkg_manager, :to => :definer
+
     private
+
+    # This probably should be elsewhere, because it only works on DepRunners that
+    # define #provides.
+    def cmds_in_path? commands = provides
+      present, missing = commands.partition {|cmd_name| cmd_dir(cmd_name) }
+      good, bad = present.partition {|cmd_name| pkg_manager.cmd_in_path? cmd_name }
+
+      log_ok "#{good.map {|i| "'#{i}'" }.to_list} run#{'s' if good.length == 1} from #{cmd_dir(good.first)}." unless good.empty?
+      log_error "#{missing.map {|i| "'#{i}'" }.to_list} #{missing.length == 1 ? 'is' : 'are'} missing from your PATH." unless missing.empty?
+
+      unless bad.empty?
+        path_error = "#{bad.map {|i| "'#{i}'" }.to_list} incorrectly run#{'s' if bad.length == 1} from #{cmd_dir(bad.first)}."
+        unless /#{pkg_manager.bin_path}.*#{cmd_dir(bad.first)}/ =~ ENV['PATH']
+          log_error path_error
+          # The incorrect paths were caused by path order, not just binaries missing from the correct path.
+          unless pkg_manager == Babushka::BaseHelper
+            # Don't recommend putting the system path ahead of manager-specific paths.
+            log "You need to put #{pkg_manager.bin_path} before #{cmd_dir(bad.first)} in your PATH."
+          end
+          :fail
+        else
+          log path_error
+        end
+      else
+        missing.empty?
+      end
+    end
 
     def dmg url, &block
       download url
--- lib/babushka/dep_runners/pkg_dep_runner.rb
+++ lib/babushka/dep_runners/pkg_dep_runner.rb
@@ -1,8 +1,6 @@
 module Babushka
   class PkgDepRunner < BaseDepRunner
 
-    delegate :pkg_manager, :to => :definer
-
     private
 
     def applicable?
@@ -19,22 +17,6 @@ module Babushka
 
     def packages_present?
       installs.all? {|pkg| pkg_manager.has? pkg }
-    end
-
-    def cmds_in_path?
-      present, missing = provides.partition {|cmd_name| cmd_dir(cmd_name) }
-      good, bad = present.partition {|cmd_name| pkg_manager.cmd_in_path? cmd_name }
-
-      log_ok "#{good.map {|i| "'#{i}'" }.to_list} run#{'s' if good.length == 1} from #{cmd_dir(good.first)}." unless good.empty?
-      log_error "#{missing.map {|i| "'#{i}'" }.to_list} #{missing.length == 1 ? 'is' : 'are'} missing from your PATH." unless missing.empty?
-
-      unless bad.empty?
-        log_error "#{bad.map {|i| "'#{i}'" }.to_list} incorrectly run#{'s' if bad.length == 1} from #{cmd_dir(bad.first)}."
-        log "You need to put #{pkg_manager.bin_path} before #{cmd_dir(bad.first)} in your PATH."
-        :fail
-      else
-        missing.empty?
-      end
     end
 
     def setup_for_install
