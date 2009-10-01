9592cc0bf281fe25ab76a7dc3b9e76bf1e4e7145
Ben Hoskings <ben@hoskings.net>
2009-10-01 19:18:44 +1000

    Use sudo in 'admins can sudo' and 'admin group' deps.

--- deps/system.rb
+++ deps/system.rb
@@ -38,12 +38,12 @@ end
 dep 'admins can sudo' do
   requires 'admin group'
   met? { !sudo('cat /etc/sudoers').split("\n").grep(/^%admin/).empty? }
-  meet { append_to_file '%admin  ALL=(ALL) ALL', '/etc/sudoers' }
+  meet { append_to_file '%admin  ALL=(ALL) ALL', '/etc/sudoers', :sudo => true }
 end
 
 dep 'admin group' do
   met? { grep /^admin\:/, '/etc/group' }
-  meet { shell "groupadd admin" }
+  meet { sudo 'groupadd admin' }
 end
 
 dep 'build tools' do
