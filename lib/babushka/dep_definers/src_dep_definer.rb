7c1e5fae899dad3b73912cc1428dffb12c5a4b06
Ben Hoskings <ben@hoskings.net>
2009-10-07 03:09:32 +1100

    Handle 'make install' in SrcHelper, so we can use #should_sudo!.

--- lib/babushka/dep_definers/src_dep_definer.rb
+++ lib/babushka/dep_definers/src_dep_definer.rb
@@ -12,7 +12,7 @@ module Babushka
     accepts_list_for :configure_args
 
     accepts_block_for(:build) { shell "make" }
-    accepts_block_for(:install) { sudo "make install" }
+    accepts_block_for(:install) { pkg_manager.install_src! 'make install' }
 
     def pkg_manager
       SrcHelper
--- lib/babushka/pkg_helpers/src_helper.rb
+++ lib/babushka/pkg_helpers/src_helper.rb
@@ -3,6 +3,10 @@ module Babushka
   class << self
     def pkg_type; :src end
 
+    def install_src! cmd
+      shell cmd, :sudo => should_sudo?
+    end
+
     def prefix
       '/usr/local'
     end
