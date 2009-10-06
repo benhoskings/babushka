dde7e3b82c7049442fc5874a5ed9dc72e9d28120
Ben Hoskings <ben@hoskings.net>
2009-10-07 01:31:03 +1100

    Added 'fake json gem' for ruby1.9 gems support.

--- deps/ruby and rubygems.rb
+++ deps/ruby and rubygems.rb
@@ -1,5 +1,34 @@
 dep 'rubygems' do
   requires 'rubygems installed', 'github source'
+  setup {
+    shell('ruby --version')['ruby 1.9'].nil? || definer.requires('fake json gem')
+  }
+end
+
+dep 'fake json gem' do
+  met? { Babushka::GemHelper.has? 'json' }
+  meet {
+    log "json is now included in ruby core, and when gems try to install the"
+    log "gem, it fails to build. So, let's install a fake version of json-1.1.9."
+    in_build_dir {
+      log_block "Generating fake json-1.1.9 gem" do
+        File.open 'fake_json.gemspec', 'w' do |f|
+          f << %Q{
+            spec = Gem::Specification.new do |s|
+              s.name = "json"
+              s.version = "1.1.9"
+              s.summary = "this fakes json (which is now included in stdlib)"
+              s.homepage = "http://gist.github.com/gists/58071"
+              s.has_rdoc = false
+              s.required_ruby_version = '>= 1.9.1'
+            end
+          }
+        end
+        shell "gem build fake_json.gemspec"
+      end
+      Babushka::GemHelper.install! 'json-1.1.9.gem', '--no-ri --no-rdoc'
+    }
+  }
 end
 
 dep 'github source' do
