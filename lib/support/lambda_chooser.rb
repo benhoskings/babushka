d38730475ad3ff4fa822d902cbe1016b59806caf
Ben Hoskings <ben@hoskings.net>
2009-10-10 22:29:10 +1100

    Updated LambdaChooser to accept choices via a single method (defaulting to #on).

--- lib/support/lambda_chooser.rb
+++ lib/support/lambda_chooser.rb
@@ -1,31 +1,29 @@
 module Babushka
   class LambdaChooser
 
-    def initialize &block
+    def initialize *choices, &block
+      @choices = choices
       @block = block
       @results = {}
     end
 
-    def choose choice, choices
-      setup choices
+    def choose choice, method_name = nil
+      self.class.send :alias_method, (method_name || :on), :process_choice
       instance_eval &@block
       @results[choice]
     end
 
-    def setup choices
-      choices.map {|choice|
-        instance_eval <<-LOL
-          def #{choice} first = nil, *rest, &block
-            @results[#{choice.inspect}] = if block_given?
-              block
-            elsif first.is_a? Hash
-              first
-            else
-              [*first].concat(rest)
-            end
-          end
-        LOL
-      }
+    def process_choice choice, first = nil, *rest, &block
+      raise "You can supply values or a block, but not both." if first && block
+      raise "The choice '#{choice}' isn't valid." unless choice.in? @choices
+
+      @results[choice] = if block
+        block
+      elsif first.is_a? Hash
+        first
+      else
+        [*first].concat(rest)
+      end
     end
 
   end
