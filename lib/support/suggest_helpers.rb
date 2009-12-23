4e5ef76a03e631e82caa3355cc1668d9cc42b67f
Ben Hoskings <ben@hoskings.net>
2009-12-23 17:07:15 +1100

    In #suggest_value_for, return false instead of nil when the user rejects a typo correction.

--- lib/support/suggest_helpers.rb
+++ lib/support/suggest_helpers.rb
@@ -8,7 +8,7 @@ module Babushka
       elsif possible_matches.length == 1
         confirm "#{"Did you mean".colorize('grey')} '#{possible_matches.first}'#{"?".colorize('grey')}" do
           possible_matches.first
-        end
+        end or false
       else
         log "Similar: #{possible_matches.map {|d| "'#{d}'" }.join(', ')}"
         prompt_for_value("Did you mean any of those".colorize('grey'), :default => possible_matches.first)
