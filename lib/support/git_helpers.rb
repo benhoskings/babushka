239d2286dc69355ad6a5a4a75cddf02bb52555b2
Ben Hoskings <ben@hoskings.net>
2009-12-27 14:50:20 +1100

    Improve logging in #git helper - log stdout on success and stderr on failure.

--- lib/support/git_helpers.rb
+++ lib/support/git_helpers.rb
@@ -5,7 +5,15 @@ module Babushka
       repo = opts[:dir] || build_path_for(uri)
       in_dir opts[:prefix] || SrcPrefix, :create => true do
         update_success = if File.directory? repo / '.git'
-          in_dir(repo) { log_shell "Updating from #{uri}", %Q{git pull origin master} }
+          in_dir(repo) {
+            log uri do
+              shell 'git pull origin master' do |shell|
+                returning shell.ok? do |result|
+                  log result ? shell.stdout : shell.stderr
+                end
+              end
+            end
+          }
         else
           log_shell "Cloning from #{uri}", %Q{git clone "#{uri}" "./#{repo}"}
         end
