b34561220ed2f8a01b3508ec941a2b6f3e551f44
Ben Hoskings <ben@hoskings.net>
2010-01-18 16:57:08 +1100

    Added String#val_for specs.

---
+++ spec/babushka/core_patches_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_support'
+
+describe String, "val_for" do
+  it "space separation" do
+    'key value'.val_for('key').should == 'value'
+  end
+  it "colon separation" do
+    'key: value'.val_for('key').should == 'value'
+  end
+  it "whitespace" do
+    '  key value '.val_for('key').should == 'value'
+  end
+  it "whitespace in key" do
+    'space-separated key: value'.val_for('space-separated key').should == 'value'
+  end
+  it "whitespace in value" do
+    'key: space-separated value'.val_for('key').should == 'space-separated value'
+  end
+  it "whitespace in both" do
+    'key with spaces: space-separated value'.val_for('key with spaces').should == 'space-separated value'
+  end
+  it "trailing characters" do
+    'key: value;'.val_for('key').should == 'value'
+  end
+end
