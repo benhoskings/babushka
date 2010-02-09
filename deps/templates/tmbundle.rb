35d31bc75c23c81a7692c2f1e972a87bd8d14b9e
Ben Hoskings <ben@hoskings.net>
2010-02-10 02:28:11 +1100

    Removed bundle_name from tmbundle, to simplify things for now.

--- deps/templates/tmbundle.rb
+++ deps/templates/tmbundle.rb
@@ -1,15 +1,14 @@
 meta :tmbundle, :for => :osx do
   accepts_list_for :source
-  accepts_list_for :bundle_name, :name
 
   template {
     requires 'TextMate.app'
     helper :path do
-      '~/Library/Application Support/TextMate/Bundles' / bundle_name
+      '~/Library/Application Support/TextMate/Bundles' / name
     end
     met? { path.dir? }
     before { shell "mkdir -p #{path.parent}" }
-    meet { git source, :dir => bundle_name.first, :prefix => path.parent }
+    meet { git source, :dir => name, :prefix => path.parent }
     after { shell %Q{osascript -e 'tell app "TextMate" to reload bundles'} }
   }
 end
