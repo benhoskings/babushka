239762a684c5cb9ea77bc006b99835363a307efe
Ben Hoskings <ben@hoskings.net>
2010-03-08 21:19:06 +1100

    Added :installer meta dep, for OS X .pkg installers.

---
+++ deps/templates/installer.rb
@@ -0,0 +1,29 @@
+meta :installer do
+  accepts_list_for :source
+  accepts_list_for :extra_source
+  accepts_list_for :provides, :name, :choose_with => :via
+  accepts_list_for :pkg_name, :name
+
+  template {
+    prepare { setup_source_uris }
+    met? { provided? }
+
+    # At the moment, we just try to install every .pkg in the archive. If that's not
+    # what you want, specify the name of the .pkg to choose from the archive using
+    # the +pkg_name+ method when you define your dep:
+    #
+    # installer 'blah' do
+    #   source 'http://blah.org/blah-latest.dmg
+    #   pkg_name 'blah-1.7.0.pkg'
+    #   provides 'blah'
+    # end
+    #
+    meet {
+      process_sources {|archive|
+        Dir.glob("**/*.pkg").map {|entry|
+          log_shell "Installing #{entry}", "installer -target / -pkg '#{entry}'", :sudo => true
+        }
+      }
+    }
+  }
+end
