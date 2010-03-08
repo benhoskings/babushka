0e5c6a2b8127886689b4a9b76d8bd91e9d0659a0
Ben Hoskings <ben@hoskings.net>
2010-03-08 21:19:15 +1100

    Use a .pkg to install git on OS X, so bootstraps don't require build tools, homebrew or a compile.

--- deps/git.rb
+++ deps/git.rb
@@ -1,7 +1,25 @@
-pkg 'git' do
+dep 'git' do
+  requires {
+    on :osx, 'installer git'
+    on :linux, 'pkg git'
+  }
+end
+
+pkg 'pkg git' do
   installs {
     via :apt, 'git-core'
     via :brew, 'git'
     via :macports, 'git-core +svn +bash_completion'
+  }
+  provides 'git'
+end
+
+installer 'installer git' do
+  source "http://git-osx-installer.googlecode.com/files/git-1.7.0-intel-leopard.dmg"
+  provides 'git'
+  after {
+     in_dir '/usr/local/bin' do
+       sudo "ln -sf /usr/local/git/bin/git* ." unless 'git'.p.exists?
+     end
   }
 end
