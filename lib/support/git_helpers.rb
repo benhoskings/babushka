bd1a7d9094dfed7ea2704eb3985aabab477bebdf
Ben Hoskings <ben@hoskings.net>
2010-03-13 19:01:55 +1100

    Redesigned GitHelpers#git to handle pre-existing paths properly.

--- lib/support/git_helpers.rb
+++ lib/support/git_helpers.rb
@@ -2,25 +2,21 @@ module Babushka
   module GitHelpers
 
     def git uri, opts = {}, &block
-      repo = opts[:dir] || File.basename(uri.to_s)
       in_dir opts[:prefix] || SrcPrefix, :create => true do
-        update_success = if File.directory? repo / '.git'
-          in_dir(repo) {
-            log uri do
-              [
-                'git fetch origin',
-                "git reset --hard origin/#{current_branch}"
-              ].all? {|cmd|
-                shell cmd do |shell|
-                  returning shell.ok? do |result|
-                    log result ? shell.stdout : shell.stderr
-                  end
-                end
-              }
-            end
-          }
+        repo = (opts[:dir] || File.basename(uri.to_s)).p
+        update_success = if !repo.exists?
+          git_clone uri, repo.basename
+        elsif !repo.dir? # exists, but not a dir...
+          if repo.empty? # ...delete and clone if it's empty
+            repo.rm
+            git_clone uri, repo.basename
+          else
+            log_error "#{repo} exists, but is not a directory."
+          end
+        elsif !(repo / '.git').exists?
+          log_error "#{repo} exists, but is not git repo."
         else
-          log_shell "Cloning from #{uri}", %Q{git clone "#{uri}" "#{'.' / repo}"}
+          git_update uri, repo
         end
 
         returning update_success do
@@ -28,6 +24,25 @@ module Babushka
           block.nil? || in_dir(repo) {|path| block.call path }
         end
       end
+    end
+
+    private
+
+    def git_clone uri, repo
+      log_shell "Cloning from #{uri}", %Q{git clone "#{uri}" "#{'.' / repo}"}
+    end
+
+    def git_update uri, repo
+      in_dir(repo) {
+        log "Updating #{uri}" do
+          [
+            'git fetch origin',
+            "git reset --hard origin/#{current_branch}"
+          ].all? {|cmd|
+            shell cmd
+          }
+        end
+      }
     end
 
     def current_branch path = nil
