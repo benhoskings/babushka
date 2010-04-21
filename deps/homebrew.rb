75bcc445bff67fe3c51717ac188b45467574d6b0
Ben Hoskings <ben@hoskings.net>
2010-04-21 22:06:44 +1000

    Added var(:homebrew_prefix), defaulting to :install_prefix.

--- deps/homebrew.rb
+++ deps/homebrew.rb
@@ -2,7 +2,7 @@ dep 'homebrew binary in place' do
   requires 'homebrew installed'
   met? { which 'brew' }
   meet {
-    in_dir var :install_prefix do
+    in_dir var :homebrew_prefix do
       log_shell "Resetting to HEAD", "git reset --hard"
     end
   }
@@ -10,16 +10,17 @@ end
 
 dep 'homebrew installed' do
   requires_when_unmet 'writable install location', 'git'
+  define_var :homebrew_prefix, :default => :install_prefix, :message => "Where would you like homebrew installed"
   define_var :homebrew_repo_user, :default => 'mxcl', :message => "Whose homebrew repo would you like to use?"
   setup {
     if Babushka::BrewHelper.present?
-      set :install_prefix, Babushka::BrewHelper.prefix # Use the existing homebrew install if there is one
+      set :homebrew_prefix, Babushka::BrewHelper.prefix # Use the existing homebrew install if there is one
     end
   }
-  met? { File.exists? var(:install_prefix) / '.git' }
+  met? { File.exists? var(:homebrew_prefix) / '.git' }
   meet {
     git "git://github.com/#{var :homebrew_repo_user}/homebrew.git" do |path|
-      log_shell "Gitifying #{var :install_prefix}", "cp -r .git '#{var :install_prefix}'"
+      log_shell "Gitifying #{var :homebrew_prefix}", "cp -r .git '#{var :homebrew_prefix}'"
     end
   }
 end
