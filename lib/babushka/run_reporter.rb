b6ec2e00e3d50fa63e8e27d177206c01a8d80aea
Ben Hoskings <ben@hoskings.net>
2010-05-08 20:54:52 +1000

    Report runs to localhost:3000 when they complete.

---
+++ lib/babushka/run_reporter.rb
@@ -0,0 +1,29 @@
+module Babushka
+  class RunReporter
+  class << self
+    def report dep, result, reportable
+      post_report dep.name, 'TODO', (reportable ? 'error' : (result ? 'ok' : 'fail'))
+    end
+
+
+    private
+
+    def post_report dep_name, source_url, result
+      require 'net/http'
+      require 'uri'
+
+      returning(Net::HTTP.post_form(
+        URI.parse('http://localhost:3000/runs.json'),
+        {
+          "dep_name" => dep_name,
+          "source_url" => source_url,
+          "result" => result
+        }
+      )) do |response|
+        log "Anonymous report: #{response.class}: #{response.body}"
+      end.is_a? Net::HTTPSuccess
+    end
+
+  end
+  end
+end
