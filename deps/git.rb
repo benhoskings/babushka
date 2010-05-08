dae78556e8da16f437f4c28946a7e0eeaabaa7b9
Ben Hoskings <ben@hoskings.net>
2010-05-08 15:52:02 +1000

    Always symlink the installer git binaries after install, since if it was unmet they can't have been there already.

--- deps/git.rb
+++ deps/git.rb
@@ -20,7 +20,7 @@ installer 'installer git' do
   provides 'git'
   after {
     in_dir '/usr/local/bin' do
-      sudo "ln -sf /usr/local/git/bin/git* ." unless 'git'.p.exists?
+      sudo "ln -sf /usr/local/git/bin/git* ."
     end
   }
 end
