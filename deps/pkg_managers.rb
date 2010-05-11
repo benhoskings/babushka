e760838616712a7361c306734e64809457181004
Ben Hoskings <ben@hoskings.net>
2010-05-11 17:05:05 +1000

    Require that the 'main' and 'universe' apt sources are present.

--- deps/pkg_managers.rb
+++ deps/pkg_managers.rb
@@ -7,9 +7,35 @@ src 'macports' do
 end
 
 ext 'apt' do
+  requires 'main apt source', 'universe apt source'
   if_missing 'apt-get' do
     log "Your system doesn't seem to have Apt installed. Is it Debian-based?"
   end
+end
+
+meta :apt_source do
+  accepts_list_for :source_name
+  template {
+    met? {
+      source_name.all? {|name|
+        grep(/^deb .* #{Babushka::Base.host.name} (\w+ )*#{Regexp.escape(name.to_s)}/, '/etc/apt/sources.list')
+      }
+    }
+    before { Babushka::AptHelper.source_for_system }
+    meet {
+      source_name.each {|name|
+        append_to_file "deb #{Babushka::AptHelper.source_for_system} #{Babushka::Base.host.name} #{name}", '/etc/apt/sources.list', :sudo => true
+      }
+    }
+  }
+end
+
+apt_source 'main apt source' do
+  source_name 'main'
+end
+
+apt_source 'universe apt source' do
+  source_name 'universe'
 end
 
 dep 'homebrew' do
