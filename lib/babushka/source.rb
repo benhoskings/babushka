14561d37b1a53bd60c85e85ef9549dc4780cd62d
Ben Hoskings <ben@hoskings.net>
2010-05-31 01:07:31 +1000

    Made Source#raise_unless_addable! a private method.

--- lib/babushka/source.rb
+++ lib/babushka/source.rb
@@ -157,21 +157,6 @@ module Babushka
       type == other.type
     end
 
-    def raise_unless_addable!
-      uri_dup_source = Base.sources.current.detect {|s|
-        "#{s.uri}==#{uri}".taph
-        s.uri == uri
-      }.taph
-      name_dup_source = Base.sources.current.detect {|s|
-        "#{s.name}==#{name}".taph
-        s.name == name
-      }.taph
-      if uri_dup_source != name_dup_source
-        raise "There is already a source called '#{name_dup_source.name}' (it contains #{name_dup_source.uri})." unless name_dup_source.nil?
-        raise "The source #{uri_dup_source.uri} is already present (as '#{uri_dup_source.name}')." unless uri_dup_source.nil?
-      end
-    end
-
     def add!
       if !cloneable?
         log "Nothing to add for #{name}."
@@ -235,6 +220,21 @@ module Babushka
     include GitHelpers
     def pull!
       git uri, :prefix => prefix, :dir => name, :log => true
+    end
+
+    def raise_unless_addable!
+      uri_dup_source = Base.sources.current.detect {|s|
+        "#{s.uri}==#{uri}".taph
+        s.uri == uri
+      }.taph
+      name_dup_source = Base.sources.current.detect {|s|
+        "#{s.name}==#{name}".taph
+        s.name == name
+      }.taph
+      if uri_dup_source != name_dup_source
+        raise "There is already a source called '#{name_dup_source.name}' (it contains #{name_dup_source.uri})." unless name_dup_source.nil?
+        raise "The source #{uri_dup_source.uri} is already present (as '#{uri_dup_source.name}')." unless uri_dup_source.nil?
+      end
     end
 
     def add_source
