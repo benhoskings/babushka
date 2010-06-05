ffb8e9daf811c182dfa64284f458ed4eea49322f
Ben Hoskings <ben@hoskings.net>
2010-06-05 19:45:30 +1000

    Converted :install_prefix var to :install_path.

--- deps/babushka.rb
+++ deps/babushka.rb
@@ -1,7 +1,7 @@
 
 dep 'babushka' do
   requires 'babushka in path', 'babushka up to date', 'dep source'
-  define_var :install_prefix, :default => '/usr/local', :message => "Where would you like babushka installed"
+  define_var :install_path, :default => '/usr/local/babushka', :message => "Where would you like babushka installed"
   define_var :babushka_branch,
     :message => "Which branch would you like to update from?",
     :default => 'master',
@@ -10,7 +10,7 @@ dep 'babushka' do
       'next' => 'The development head -- slight risk of explosions'
     }
   setup {
-    set :install_prefix, Babushka::Path.prefix if Babushka::Path.run_from_path?
+    set :install_path, Babushka::Path.path if Babushka::Path.run_from_path?
     set :babushka_branch, 'master'
   }
 end
@@ -18,22 +18,22 @@ end
 dep 'babushka up to date' do
   requires 'babushka repo clean', 'babushka update would fast forward'
   met? {
-    in_dir(var(:install_prefix) / 'babushka') {
+    in_dir(var(:install_path)) {
       shell("git rev-list ..origin/#{var :babushka_branch}").lines.to_a.empty?
     }
   }
-  meet { in_dir(var(:install_prefix) / 'babushka') { shell("git merge origin/#{var :babushka_branch}", :log => true) } }
+  meet { in_dir(var(:install_path)) { shell("git merge origin/#{var :babushka_branch}", :log => true) } }
 end
 
 dep 'babushka update would fast forward' do
   requires 'babushka installed'
   met? {
-    in_dir(var(:install_prefix) / 'babushka') {
+    in_dir(var(:install_path)) {
       if !shell('git fetch')
         fail_because("Couldn't pull the latest code - check your internet connection.")
       else
         shell("git rev-list origin/#{var :babushka_branch}..").lines.to_a.empty? or
-        fail_because("There are unpushed commits in #{var(:install_prefix) / 'babushka'}.")
+        fail_because("There are unpushed commits in #{var(:install_path)}.")
       end
     }
   }
@@ -42,9 +42,9 @@ end
 dep 'babushka repo clean' do
   requires 'babushka installed'
   met? {
-    in_dir(var(:install_prefix) / 'babushka') {
+    in_dir(var(:install_path)) {
       shell('git ls-files -m').lines.to_a.empty? or
-      fail_because("There are local changes in #{var(:install_prefix) / 'babushka'}.")
+      fail_because("There are local changes in #{var(:install_path)}.")
     }
   }
 end
@@ -53,7 +53,7 @@ dep 'babushka in path' do
   requires 'babushka installed'
   met? { which 'babushka' }
   meet {
-    log_shell "Linking babushka into #{var(:install_prefix) / 'bin'}", %Q{ln -sf "#{var(:install_prefix) / 'babushka/bin/babushka.rb'}" "#{var(:install_prefix) / 'bin/babushka'}"}
+    log_shell "Linking babushka into #{var(:install_path) / '../bin'}", %Q{ln -sf "#{var(:install_path) / 'bin/babushka.rb'}" "#{var(:install_path) / '../bin/babushka'}"}
   }
 end
 
@@ -73,10 +73,10 @@ end
 dep 'babushka installed' do
   requires 'ruby', 'git'
   setup { set :babushka_source, "git://github.com/benhoskings/babushka.git" }
-  met? { git_repo?(var(:install_prefix) / 'babushka') }
+  met? { git_repo?(var(:install_path)) }
   meet {
-    in_dir var :install_prefix do |path|
-      log_shell "Installing babushka to #{var(:install_prefix) / 'babushka'}", %Q{git clone "#{var :babushka_source}" ./babushka}
+    in_dir var(:install_path).p.parent do |path|
+      log_shell "Installing babushka to #{var(:install_path)}", %Q{git clone "#{var :babushka_source}" ./babushka}
     end
   }
 end
