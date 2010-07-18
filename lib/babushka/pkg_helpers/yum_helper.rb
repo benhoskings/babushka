87a5c6ce3973d0447b4f115aef9aebcaca3d3922
Ben Hoskings <ben@hoskings.net>
2010-07-18 16:29:06 +1000

    Added YumHelper#_has?.

--- lib/babushka/pkg_helpers/yum_helper.rb
+++ lib/babushka/pkg_helpers/yum_helper.rb
@@ -5,6 +5,13 @@ module Babushka
     def pkg_cmd; pkg_binary end
     def pkg_binary; "yum" end
     def manager_key; :yum end
+
+    private
+
+    def _has? pkg_name
+      failable_shell("#{pkg_binary} list '#{pkg_name}'").stdout.val_for(/^#{pkg_name}\.(\w+)/).ends_with?('installed')
+    end
+
   end
   end
 end
