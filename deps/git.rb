07b915be94d8fe2a726d7d2fd083ddb848ce1803
Ben Hoskings <ben@hoskings.net>
2010-07-20 12:19:53 +1000

    Renamed 'usr-local subpaths exist' -> 'usr-local.install_path' in 'git.installer' require.

--- deps/git.rb
+++ deps/git.rb
@@ -16,7 +16,7 @@ dep 'git.managed' do
 end
 
 dep 'git.installer' do
-  requires_when_unmet 'usr-local subpaths exist'
+  requires_when_unmet 'usr-local.install_path'
   source "http://git-osx-installer.googlecode.com/files/git-1.7.1-intel-leopard.dmg"
   provides 'git'
   after {
