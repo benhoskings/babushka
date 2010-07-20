420d34461b4d4adfc91815e3cba6223621033ec4
Ben Hoskings <ben@hoskings.net>
2010-07-20 17:45:17 +1000

    Re-added :service_name and :cfg accepts_list_for to the 'gem' meta dep, after conversion from GemDepDefiner.

--- deps/templates/managed.rb
+++ deps/templates/managed.rb
@@ -68,6 +68,8 @@ end
 meta :gem do
   accepts_list_for :installs, :default_pkg, :choose_with => :via
   accepts_list_for :provides, :default_pkg, :choose_with => :via
+  accepts_list_for :service_name, :name
+  accepts_list_for :cfg
 
   def default_pkg
     VersionOf.new name
