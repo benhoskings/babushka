90b11da587b200588332ff9379d383c07a798d4f
Ben Hoskings <ben@hoskings.net>
2010-08-18 00:28:52 +1000

    Namespace Verb, Opt, Arg and friends within Babushka::Base instead of just Babushka.

--- lib/babushka/structs.rb
+++ lib/babushka/structs.rb
@@ -1,8 +1,10 @@
 module Babushka
-  Verb = Struct.new :name, :short, :long, :description, :opts, :args
-  Opt = Struct.new :name, :short, :long, :description, :optional, :args
-  Arg = Struct.new :name, :description, :optional, :multi, :example
-  PassedVerb = Struct.new :def, :opts, :args
-  PassedOpt = Struct.new :def, :args 
-  PassedArg = Struct.new :def, :value
+  class Base
+    Verb = Struct.new :name, :short, :long, :description, :opts, :args
+    Opt = Struct.new :name, :short, :long, :description, :optional, :args
+    Arg = Struct.new :name, :description, :optional, :multi, :example
+    PassedVerb = Struct.new :def, :opts, :args
+    PassedOpt = Struct.new :def, :args
+    PassedArg = Struct.new :def, :value
+  end
 end
