8e988a73b09bbae25610faec182cf6f8455180ff
Ben Hoskings <ben@hoskings.net>
2010-08-18 16:31:39 +1000

    Redesigned the babushka load process.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,10 +1,34 @@
 module Babushka
-  VERSION = '0.6.2'
+  VERSION        = '0.6.2'
+  WorkingPrefix  = '~/.babushka'
+  SourcePrefix   = '~/.babushka/sources'
+  BuildPrefix    = '~/.babushka/build'
+  DownloadPrefix = '~/.babushka/downloads'
+  LogPrefix      = '~/.babushka/logs'
+  VarsPrefix     = '~/.babushka/vars'
 
-  WorkingPrefix = '~/.babushka'
-  SourcePrefix = WorkingPrefix / 'sources'
-  BuildPrefix = WorkingPrefix / 'build'
-  DownloadPrefix = WorkingPrefix / 'downloads'
-  LogPrefix = WorkingPrefix / 'logs'
-  VarsPrefix = WorkingPrefix / 'vars'
+  module Path
+    def self.binary() File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__ end
+    def self.bin() File.dirname(binary) end
+    def self.path() File.dirname(bin) end
+    def self.run_from_path?() ENV['PATH'].split(':').include? File.dirname($0) end
+  end
 end
+
+# First, load the component lists.
+require File.join(Babushka::Path.path, 'lib', 'components')
+
+# Load external components that babushka depends on.
+Babushka::ExternalComponents.each {|c| require File.join(Babushka::Path.path, 'lib', c) }
+
+# Next, load babushka itself.
+Babushka::Components.each {|c| require File.join(Babushka::Path.path, 'lib/babushka', c) }
+
+# Finally, mix in some top-level helper methods:
+Object.class_eval {
+  # Dep helpers like #Dep, #dep & #meta;
+  include Babushka::Dep::Helpers
+  # Logging helpers like #log, #log_ok, #log_error & #debug.
+  include Babushka::Logger::Helpers
+  include Babushka::VersionOf::Helpers
+}
