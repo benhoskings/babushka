89b55c89f435d3c18c7638c9c4c9964797f4721e
Ben Hoskings <ben@hoskings.net>
2010-08-29 15:54:46 +0900

    Added Numeric#commas rdocs.

--- lib/babushka/core_patches/numeric.rb
+++ lib/babushka/core_patches/numeric.rb
@@ -1,4 +1,10 @@
 class Numeric
+  # Return a string representation of this value with commas between the
+  # thousands groupings.
+  # Some examples:
+  # 3.commas     #=> "3"
+  # 314.commas   #=> "314"
+  # 31459.commas #=> "31,459"
   def commas
     if self < 1000
       to_s
