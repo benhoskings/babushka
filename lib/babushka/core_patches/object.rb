c6a949307078cfed19b248d1fcec80649ba4e879
Ben Hoskings <ben@hoskings.net>
2010-08-29 16:27:10 +0900

    Added Object rdocs.

--- lib/babushka/core_patches/object.rb
+++ lib/babushka/core_patches/object.rb
@@ -1,15 +1,24 @@
 alias :L :lambda
 
 class Object
+  # Yield the supplied object to the block before returning it. This is useful
+  # for writing cleaner object creations and related things. For example:
+  #   returning [] do |list|
+  #     list << 'value' if condition
+  #   end
   def returning obj, &block
     yield obj
     obj
   end
 
+  # The opposite of Array#include - i.e. return true if self appears in the
+  # array splatted into +first+ and +rest+.
   def in? first, *rest
     (first.is_a?(Array) ? first : [first].concat(rest)).include? self
   end
 
+  # Return this object's metaclass; i.e. the value of self within a
+  # 'class << self' block.
   def metaclass
     class << self; self end
   end
@@ -22,16 +31,30 @@ class Object
     name.split('::').inject(Object) {|klass,name| klass.const_get name }
   end
 
+  # Return true if this object is +nil?+, or +empty?+ if it accepts that method.
   def blank?
     nil? || (respond_to?(:empty?) && empty?)
   end
 
+  # Return this object after yielding it to the block. This is useful for
+  # easily inserting logging, among other things. Given this code:
+  #   values.map(&:name).join(', ')
+  # You can easily insert logging like so:
+  #   values.map(&:name).tap {|i| log i }.join(', ')
   def tap &block
     returning(self) { yield self }
   end
+
+  # Return self unmodified after logging the output of #inspect, along with
+  # the point at which +tapp+ was called.
   def tapp
     tap { puts "#{File.basename caller[4]}: #{self.inspect}" }
   end
+  # Log and return unmodified in the same manner as #tapp, but escape the
+  # output to be HTML safe and easily readable. For example,
+  #   #<Object:0x00000100bda208>
+  # becomes
+  #   #&lt;Object:0x00000100bda208><br />
   def taph
     tap { puts("#{File.basename caller[4]}: #{self.inspect}".gsub('&', '&amp;').gsub('<', '&lt;') + "<br />") }
   end
@@ -49,6 +72,14 @@ unless Object.new.respond_to? :instance_exec
   class Object
     module InstanceExecHelper; end
     include InstanceExecHelper
+
+    # Executes the given block within the context of the receiver. In
+    # order to set the context, the variable self is set to this object
+    # while the block is executing, giving the code access to this object's
+    # instance variables. Arguments are passed as block parameters.
+    #
+    # This is a fallback implementation for older rubies that don't have
+    # a built-in #instance_exec.
     def instance_exec(*args, &block)
       begin
         old_critical, Thread.critical = Thread.critical, true
