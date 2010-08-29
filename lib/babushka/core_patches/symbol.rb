3b37537ec71548b8db9748e678b8faa30747b0a3
Ben Hoskings <ben@hoskings.net>
2010-08-29 16:31:30 +0900

    Added Symbol#to_proc rdocs.

--- lib/babushka/core_patches/symbol.rb
+++ lib/babushka/core_patches/symbol.rb
@@ -1,5 +1,10 @@
 unless :to_proc.respond_to? :to_proc
   class Symbol
+    # Returns a Proc object which responds to a method whose name is this
+    # symbol.
+    #
+    # This is a fallback implementation for older rubies that don't have
+    # a built-in Symbol#to_proc.
     def to_proc
       L{|*args| args.shift.__send__ self, *args }
     end
