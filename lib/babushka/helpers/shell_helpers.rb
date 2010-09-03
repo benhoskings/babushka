c77b787c77a0944beab202ec5a70687164cfcf89
Ben Hoskings <ben@hoskings.net>
2010-09-03 15:39:04 +0900

    Added ShellHelpers#shell rdocs - still a couple of options to document though.

--- lib/babushka/helpers/shell_helpers.rb
+++ lib/babushka/helpers/shell_helpers.rb
@@ -1,5 +1,23 @@
 module Babushka
   module ShellHelpers
+    # Run +cmd+.
+    #
+    # If the command succeeds (i.e. returns 0), its output will be returned
+    # with a trailing newline stripped, if there was one. If the command fails
+    # (i.e. returns a non-zero value), nil will be returned.
+    #
+    # If a block is given, it will be yielded once the command has run, with a
+    # Babushka::Shell object as its sole argument. Details of the shell command
+    # are contained in this object - see the methods +cmd+, +result+, +stdout+
+    # and +stderr+.
+    #
+    # Several options can be provided to alter #shell's behaviour.
+    #   <tt>:sudo => true</tt> runs the the command as a sudo. If the command
+    #     contains piping or redirection, a 'sudo su' variant will be used
+    #     instead so that the pipe receiver or redirect targets are also
+    #     included in the sudo.
+    #   <tt>:as => 'user'</tt> causes sudo to run as the specified user instead
+    #     of root.
     def shell cmd, opts = {}, &block
       shell_method = opts.delete(:sudo) ? :sudo : :shell_cmd
       send shell_method, cmd, opts, &block
