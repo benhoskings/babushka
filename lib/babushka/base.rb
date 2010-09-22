7ee2fad7c2adea8b18cf0f8f5c1a5d504c931b09
Ben Hoskings <ben@hoskings.net>
2010-09-23 02:04:38 +1000

    Added Base.threads and .in_thread, for storing and adding thread jobs, respectively.

--- lib/babushka/base.rb
+++ lib/babushka/base.rb
@@ -28,6 +28,14 @@ module Babushka
       @sources ||= SourcePool.new
     end
 
+    def threads
+      @threads ||= []
+    end
+
+    def in_thread &block
+      threads.push Thread.new &block
+    end
+
     # The top-level entry point for babushka runs invoked at the command line.
     # When the `babushka` command is run, bin/babushka first triggers a load
     # via lib/babushka.rb, and then calls this method, passing in the
