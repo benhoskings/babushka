4ff7b6a16db5f6bf3fe9d7fe345a8d74bd43c5f5
Ben Hoskings <ben@hoskings.net>
2010-09-23 01:16:29 +1000

    Replaced Dep.process with .find_or_suggest, which just yields the dep that was found or spell-corrected instead of running it.

--- lib/babushka/dep.rb
+++ lib/babushka/dep.rb
@@ -149,16 +149,18 @@ module Babushka
       )
     end
 
-    # Look up the dep specified by +dep_name+, calling +#process+ on it if it
-    # was found. If no dep was found by the exact dep_name that was specified,
-    # suggest other similarly spelt ones in case there was a typo.
-    def self.process dep_name, with_run_opts = {}
-      if (dep = Dep(dep_name, with_run_opts)).nil?
+    # Look up the dep specified by +dep_name+, yielding it to the block if it
+    # was found.
+    #
+    # If no such dep exists, search for other similarly spelt deps and re-call
+    # this same method on the one chosen by the user, if any.
+    def self.find_or_suggest dep_name, &block
+      if (dep = Dep(dep_name)).nil?
         log "#{dep_name.to_s.colorize 'grey'} #{"<- this dep isn't defined!".colorize('red')}"
         suggestion = suggest_value_for(dep_name, Base.sources.current_names)
-        Dep.process suggestion, with_run_opts unless suggestion.nil?
+        Dep.context suggestion unless suggestion.nil?
       else
-        dep.process with_run_opts
+        block.call dep
       end
     end
 
@@ -297,7 +299,9 @@ module Babushka
 
     def process_deps accessor = :requires
       definer.send(accessor).send(task.opt(:dry_run) ? :each : :all?, &L{|dep_name|
-        Dep.process dep_name, :parent_source => dep_source
+        Dep.find_or_suggest dep_name do |dep|
+          dep.process :parent_source => dep_source
+        end
       })
     end
 
