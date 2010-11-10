ad3f409663664f42883c89864248d07d701db14f
Ben Hoskings <ben@hoskings.net>
2010-11-10 16:18:08 +0800

    Redesigned GitRepo to use shell(..., :dir) & handle nonexistent paths properly, and replaced #pushed? with #ahead?.

--- lib/babushka/git_repo.rb
+++ lib/babushka/git_repo.rb
@@ -1,4 +1,6 @@
 module Babushka
+  class GitRepoError < StandardError
+  end
   class GitRepo
     include PathHelpers
     extend PathHelpers
@@ -9,15 +11,24 @@ module Babushka
       in_dir(path) {
         maybe = shell("git rev-parse --git-dir")
         maybe.p / '..' unless maybe.nil?
-      }
+      } if path.p.dir?
     end
 
     def initialize path
+      @path = path
       @repo = self.class.repo_for(path)
     end
 
+    def repo_shell cmd
+      if repo.nil? || !repo.exists?
+        raise GitRepoError, "There is no repo at #{@path}."
+      else
+        shell cmd, :dir => repo
+      end
+    end
+
     def clean?
-      in_dir(repo) { shell("git ls-files -m").empty? }
+      repo_shell("git ls-files -m").empty?
     end
 
     def dirty?
@@ -25,26 +36,22 @@ module Babushka
     end
 
     def current_branch
-      File.read(repo / '.git/HEAD').strip.sub(/^.*refs\/heads\//, '')
+      repo_shell("cat .git/HEAD").strip.sub(/^.*refs\/heads\//, '')
     end
 
     def current_head
-      in_dir(repo) { shell("git rev-parse --short HEAD") }
+      repo_shell("git rev-parse --short HEAD")
     end
 
     def remote_branch_exists?
-      in_dir(repo) {
-        shell('git branch -a').split("\n").map(&:strip).detect {|b|
-          b[/^(remotes\/)?origin\/#{current_branch}$/]
-        }
+      repo_shell('git branch -a').split("\n").map(&:strip).detect {|b|
+        b[/^(remotes\/)?origin\/#{current_branch}$/]
       }
     end
 
-    def pushed?
-      in_dir(repo) {
-        remote_branch_exists? &&
-        shell("git rev-list origin/#{current_branch}..").split("\n").empty?
-      }
+    def ahead?
+      !remote_branch_exists? ||
+      !repo_shell("git rev-list origin/#{current_branch}..").split("\n").empty?
     end
 
     def inspect
--- spec/babushka/git_repo_spec.rb
+++ spec/babushka/git_repo_spec.rb
@@ -23,12 +23,15 @@ def stub_remote name
   end
 end
 
-def in_repo name, &block
+def repo_context name, &block
   PathSupport.in_dir(tmp_prefix / 'repos'/ name, &block)
 end
 
 describe GitRepo, 'creation' do
   before { stub_repo 'a' }
+  it "should return nil on nonexistent paths" do
+    Babushka::GitRepo.new(tmp_prefix / 'repos/nonexistent').repo.should == nil
+  end
   it "should return nil on non-repo paths" do
     Babushka::GitRepo.new(tmp_prefix / 'repos').repo.should == nil
   end
@@ -38,6 +41,15 @@ describe GitRepo, 'creation' do
   it "should find the parent when called on the subdir" do
     Babushka::GitRepo.new(tmp_prefix / 'repos/a/lib').repo.should == tmp_prefix / 'repos/a'
   end
+end
+
+describe GitRepo, 'without a repo' do
+  subject { Babushka::GitRepo.new(tmp_prefix / 'repos/nonexistent') }
+    [:clean?, :dirty?, :current_branch, :current_head, :remote_branch_exists?, :ahead?].each {|method|
+      it "should raise on #{method}" do
+        L{ subject.send(method) }.should raise_error Babushka::GitRepoError, "There is no repo at #{tmp_prefix / 'repos/nonexistent'}."
+      end
+    }
 end
 
 describe GitRepo, '#clean? / #dirty?' do
@@ -62,14 +74,14 @@ describe GitRepo, '#current_branch' do
   end
   context "after creating another branch" do
     before {
-      in_repo('a') { shell "git checkout -b next"}
+      repo_context('a') { shell "git checkout -b next"}
     }
     it "should return 'next'" do
       subject.current_branch.should == 'next'
     end
     context "after changing back to master" do
       before {
-        in_repo('a') { shell "git checkout master"}
+        repo_context('a') { shell "git checkout master"}
       }
       it "should return 'next'" do
         subject.current_branch.should == 'master'
@@ -86,12 +98,12 @@ describe GitRepo, '#current_head' do
   end
 end
 
-describe GitRepo, '#pushed?' do
+describe GitRepo, '#ahead?' do
   before { stub_repo 'a', :with_remote => true }
   subject { Babushka::GitRepo.new(tmp_prefix / 'repos/a') }
-  it "should return false if the current branch has no remote" do
+  it "should return true if the current branch has no remote" do
     subject.remote_branch_exists?.should be_false
-    subject.should_not be_pushed
+    subject.should be_ahead
   end
   context "when remote branch exists" do
     before {
@@ -103,7 +115,7 @@ describe GitRepo, '#pushed?' do
     }
     it "should return false if there are unpushed commits on the current branch" do
       subject.remote_branch_exists?.should be_true
-      subject.should_not be_pushed
+      subject.should be_ahead
     end
     context "when the branch is fully pushed" do
       before {
@@ -113,7 +125,7 @@ describe GitRepo, '#pushed?' do
       }
       it "should return true if the current branch is fully pushed" do
         subject.remote_branch_exists?.should be_true
-        subject.should be_pushed
+        subject.should_not be_ahead
       end
     end
   end
