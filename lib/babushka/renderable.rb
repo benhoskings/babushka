1039dd8b1fa53c4647fa1e8bad4f9daabe159ba2
Ben Hoskings <ben@hoskings.net>
2010-11-15 00:17:23 +0800

    Redesigned Renderable.new(source).render_to(target) to Renderable.new(path).render(source).

--- lib/babushka/renderable.rb
+++ lib/babushka/renderable.rb
@@ -2,22 +2,22 @@ module Babushka
   class Renderable
     include RunHelpers
 
-    attr_reader :source
-    def initialize source
-      @source = source
+    attr_reader :path
+    def initialize path
+      @path = path
     end
 
-    def render_to target
-      Inkan.seal(target) {|inkan|
+    def render source
+      Inkan.seal(path) {|inkan|
         inkan.credit = "Generated #{_by_babushka}"
-        inkan.print render_erb
+        inkan.print render_erb(source)
       }
     end
 
     private
 
-    def render_erb
-      ERB.new(IO.read(@source)).result(binding)
+    def render_erb source
+      ERB.new(IO.read(source)).result(binding)
     end
   end
 end
