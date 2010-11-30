af42d84a9751d63ba9893d1c077accf62499c3c6
Ben Hoskings <ben@hoskings.net>
2010-12-01 09:24:45 +1100

    Log in GitRepo#clone!.

--- lib/babushka/git_repo.rb
+++ lib/babushka/git_repo.rb
@@ -72,9 +72,11 @@ module Babushka
 
     def clone! from
       raise GitRepoExists, "Can't clone #{from} to existing path #{path}." if exists?
-      failable_shell("git clone '#{from}' '#{path.basename}'", :dir => path.parent, :create => true).tap {|shell|
-        raise GitRepoError, "Couldn't clone to #{path}: #{error_message_for shell.stderr}" unless shell.result
-      }.result
+      log_block "Cloning #{from}" do
+        failable_shell("git clone '#{from}' '#{path.basename}'", :dir => path.parent, :create => true).tap {|shell|
+          raise GitRepoError, "Couldn't clone to #{path}: #{error_message_for shell.stderr}" unless shell.result
+        }.result
+      end
     end
 
     def track! branch
