a784b7a6110836e19c0e6c25ec582b2ec0d53f06
Ben Hoskings <ben@hoskings.net>
2010-12-14 17:41:18 +1100

    Typo in AcceptsListFor spec description.

--- spec/babushka/accepts_for_spec.rb
+++ spec/babushka/accepts_for_spec.rb
@@ -103,7 +103,7 @@ end
 
 describe "accepts_list_for input processing" do
   describe "value input" do
-    it "should always return a [VersionOf] list" do
+    it "should always return a list" do
       test_lists.each_pair {|input, expected|
         list = AcceptsForTest.new
         list.records input
