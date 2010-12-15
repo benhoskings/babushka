2d1fb0c66f815c91eb34b6d6fed494e4a1342e97
Ben Hoskings <ben@hoskings.net>
2010-12-15 19:14:02 +1100

    In BaseDepRunner#provided?, convert the input to a VersionOf array.

--- lib/babushka/dep_runners/base_dep_runner.rb
+++ lib/babushka/dep_runners/base_dep_runner.rb
@@ -15,7 +15,7 @@ module Babushka
     end
 
     def provided? provided_list = provides
-      apps, commands = provided_list.partition {|i| i.name[/\.app\/?$/] }
+      apps, commands = [*provided_list].versions.partition {|i| i.name[/\.app\/?$/] }
       apps_in_path?(apps) and cmds_in_path?(commands) and matching_versions?(commands)
     end
 
