55d026ed98d0914cc482006ad45cc1a119c8f9bb
Ben Hoskings <ben@hoskings.net>
2010-12-21 18:57:21 +1100

    Converted #helper calls to standard method definitions, moving them outside the template where possible (everywhere except managed).

--- deps/templates/external.rb
+++ deps/templates/external.rb
@@ -2,10 +2,11 @@ meta :external do
   accepts_list_for :expects
   accepts_block_for :otherwise
 
+  def cmds_present? cmds
+    (cmds || []).all? {|cmd| which cmd }
+  end
+
   template {
-    helper :cmds_present? do |cmds|
-      (cmds || []).all? {|cmd| which cmd }
-    end
     met? {
       returning cmds_present?(expects) || :fail do |result|
         otherwise.call if result == :fail
