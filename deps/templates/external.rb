8e9d1defbd88b77bea5e125d5ab55e9a011916e5
Ben Hoskings <ben@hoskings.net>
2010-12-27 21:27:15 +1100

    Use #provided in external template, for proper checking of #expects.

--- deps/templates/external.rb
+++ deps/templates/external.rb
@@ -2,13 +2,9 @@ meta :external do
   accepts_list_for :expects
   accepts_block_for :otherwise
 
-  def cmds_present? cmds
-    (cmds || []).all? {|cmd| which cmd }
-  end
-
   template {
     met? {
-      returning cmds_present?(expects) || :fail do |result|
+      returning provided?(expects) || :fail do |result|
         otherwise.call if result == :fail
       end
     }
