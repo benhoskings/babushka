1702bf96016b30535ad3b54e17ca9fa8aa7e35e6
Ben Hoskings <ben@hoskings.net>
2011-01-07 17:08:31 +1100

    v0.8.3 - lazy dep defining and more structured met?/meet logging.
    - Deps are lazily defined now (they're not defined until they're run), which reduces load time and means deps kind of behave a bit like methods - the body isn't run unless the dep is invoked.
    - All the 'not already met' / '<dep name> met' logging has been removed, and instead the meet{} block is logged within its own level of indentation. This means the divide between met? / meet is clearly shown in the logging, and it makes it much clearer which deps out of a large tree were met during a run.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.8.2'
+  VERSION        = '0.8.3'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
