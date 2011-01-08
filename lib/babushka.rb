247c72776557c3c02f2b80c0d9d03e178a1b4cbc
Ben Hoskings <ben@hoskings.net>
2011-01-08 13:33:47 +1100

    v0.8.4 - fixed a stupid bug that broke some commands, including adding sources. The real problem here is no high-level tests -- babushka is well unit tested, but the higher level ones need to be there too.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.8.3'
+  VERSION        = '0.8.4'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
