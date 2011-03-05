0b8b3c5fa6a9645d6ba4ee6dc1ea1837109b3b6b
Ben Hoskings <ben@hoskings.net>
2011-03-05 15:02:46 +1100

    Added 'ppa' meta dep, from my dep source.

---
+++ deps/templates/ppa.rb
@@ -0,0 +1,24 @@
+dep 'python-software-properties.managed' do
+  provides 'add-apt-repository'
+end
+
+meta :ppa do
+  accepts_value_for :adds
+  template {
+    requires 'python-software-properties.managed'
+    met? {
+      Dir.glob("/etc/apt/sources.list.d/*").any? {|f|
+        f.p.read[Regexp.new('https?://' + adds.gsub(':', '.*') + '/ubuntu ')]
+      }
+    }
+    before {
+      adds[/^\w+\:\w+/] or log_error("'#{adds}' doesn't look like 'ppa:something'.")
+    }
+    meet {
+      sudo "sudo add-apt-repository #{adds}"
+    }
+    after {
+      Babushka::Base.host.pkg_helper.update_pkg_lists "Updating apt lists to load #{adds}."
+    }
+  }
+end
