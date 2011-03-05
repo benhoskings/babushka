80556b390d3a9b8ca95738a832257d57f33a094c
Ben Hoskings <ben@hoskings.net>
2011-03-05 14:47:20 +1100

    Bumped git.installer to v1.7.4.1.

--- deps/git.rb
+++ deps/git.rb
@@ -17,8 +17,8 @@ end
 
 dep 'git.installer' do
   requires_when_unmet 'usr-local.install_path'
-  source "http://git-osx-installer.googlecode.com/files/git-1.7.4-x86_64-leopard.dmg"
-  provides 'git >= 1.7.4'
+  source "http://git-osx-installer.googlecode.com/files/git-1.7.4.1-x86_64-leopard.dmg"
+  provides 'git >= 1.7.4.1'
   after {
     in_dir '/usr/local/bin' do
       sudo "ln -sf /usr/local/git/bin/git* ."
