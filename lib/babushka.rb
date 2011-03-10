ef6dbd3c761663f9cedc16a50d6aeb21f0d1ac77
Ben Hoskings <ben@hoskings.net>
2011-03-10 12:29:45 +1100

    v0.8.9 - Check for writability on babushka update, less verbose output, 'ppa' meta dep added to core, 'otherwise' in choice blocks, fixed two obscure exceptions thanks to Nick Partridge. `git log v0.8.8..v0.8.9` for details.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.8.8'
+  VERSION        = '0.8.9'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
