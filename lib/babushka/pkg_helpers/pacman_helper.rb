ec99d18d3158f2f6cf4e38be6284e07f449761e1
David Goodlad <david@goodlad.ca>
2011-04-12 15:44:43 +1000

    Add Arch Linux system profile and package helper

---
+++ lib/babushka/pkg_helpers/pacman_helper.rb
@@ -0,0 +1,21 @@
+module Babushka
+  class PacmanHelper < PkgHelper
+  class << self
+    def pkg_type; :package end # a description of the type of package, that makes sense in the context of, e.g. "the zsh deb"
+    def pkg_cmd; "#{pkg_binary}" end # The full command to run the package manager
+    def pkg_binary; "pacman" end # The name of the binary itself
+    def manager_key; :pacman end # How packages should be specified in deps, e.g. "via :apt, 'zsh'"
+
+    private
+
+    def _install! pkgs, opts
+      log_shell "Downloading", "#{pkg_cmd} -S --noconfirm #{pkgs.join(' ')}", :sudo => should_sudo?
+      super
+    end
+
+    def _has? pkg_name
+      failable_shell("pacman -Q #{pkg_name}").stdout !~ /not found$/
+    end
+  end
+  end
+end
