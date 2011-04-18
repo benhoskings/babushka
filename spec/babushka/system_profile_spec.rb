bf2e5f2e1f3d0521b37745bde6589dc19af8e508
Ben Hoskings <ben@hoskings.net>
2011-04-18 20:42:07 +1000

    Specced flavour-specific Linux version matching using debian.

--- spec/babushka/system_profile_spec.rb
+++ spec/babushka/system_profile_spec.rb
@@ -26,6 +26,24 @@ describe Babushka::SystemProfile, '.for_host' do
     it "should return LinuxSystemProfile on unknown Linux boxes" do
       Babushka::SystemProfile.for_host.class.should == Babushka::LinuxSystemProfile
     end
+    context "version matching" do
+      before {
+        File.should_receive(:exists?).with("/etc/debian_version").and_return(true)
+        @profile = Babushka::SystemProfile.for_host
+        @profile.should_receive(:shell).with("which lsb_release").and_return("/usr/bin/lsb_release")
+        @profile.should_receive(:shell).with("lsb_release -a").and_return(%Q{
+          No LSB modules are available.
+          Distributor ID:	Debian
+          Description:	Debian GNU/Linux 6.0.1 (squeeze)
+          Release:	6.0.1
+          Codename:	squeeze
+        }.strip)
+      }
+      it "should detect debian squeeze" do
+        @profile.match_list.should == [:squeeze, :debian, :apt, :linux, :all]
+        @profile.version.should == '6.0.1'
+      end
+    end
   end
 end
 
