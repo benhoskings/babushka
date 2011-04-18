525d4b379af3b5d2be2abf222f4754f605b2a13f
Ben Hoskings <ben@hoskings.net>
2011-04-18 15:57:37 +1000

    Added DebianSystemProfile#name, which parses the name from `lsb_release`, instead of looking it up.

--- lib/babushka/system_profile.rb
+++ lib/babushka/system_profile.rb
@@ -154,6 +154,7 @@ module Babushka
     def flavour; flavour_str.downcase.to_sym end
     def flavour_str; version_info.val_for 'Distributor ID' end
     def version; version_info.val_for 'Release' end
+    def name; version_info.val_for 'Codename' end
     def setup
       which('lsb_release') or log("Babushka uses `lsb_release` to learn about debian-based systems.") {
         AptHelper.install!('lsb-release')
