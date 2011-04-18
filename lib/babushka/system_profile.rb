3215970bfaeaad516fd01619253a45d3ee3e2f75
Ben Hoskings <ben@hoskings.net>
2011-04-18 20:39:42 +1000

    Removed the now-empty SystemProfile#setup method.

--- lib/babushka/system_profile.rb
+++ lib/babushka/system_profile.rb
@@ -11,10 +11,6 @@ module Babushka
       new
     end
 
-    def initialize
-      setup
-    end
-
     def version_info
       @_version_info ||= get_version_info
     end
@@ -22,7 +18,6 @@ module Babushka
     def linux?; false end
     def osx?; false end
     def pkg_helper; nil end
-    def setup; true end
     def pkg_helper_key; pkg_helper.try(:manager_key) end
     # The extension that dynamic libraries are given on this system. On linux
     # libraries are named like 'libssl.so'; on OS X, 'libssl.bundle'.
