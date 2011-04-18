f85d759123671d3c5abc7e06c64e1e345efd2a0a
Ben Hoskings <ben@hoskings.net>
2011-04-18 21:12:05 +1000

    v0.8.11 - new #shell helper with progress, arch linux support by @dgoodlad, better Linux support in general, VersionOf cleanups, faster specs, latest git & gem. `git log v0.8.10..v0.8.11` for details.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.8.10'
+  VERSION        = '0.8.11'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
