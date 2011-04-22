6677279453a0c06bfa0db66fff41e586d304ce06
Ben Hoskings <ben@hoskings.net>
2011-04-22 19:42:24 +1000

    Use wget instead of curl.

--- spec/acceptance/acceptance.rb
+++ spec/acceptance/acceptance.rb
@@ -5,11 +5,10 @@ require 'acceptance_helper'
 describe "babushka" do
   before(:all) {
     @vm = VM.instance
-    @vm.run 'apt-get install -qqy curl'
   }
   context "bootstrapping" do
     before(:all) {
-      @vm.run 'bash -c "`curl babushka.me/up/hard`"'
+      @vm.run 'bash -c "`wget -O - babushka.me/up/hard`"'
     }
     it "should have installed babushka" do
       @vm.run('babushka --version').should =~ /^[\d.]+$/
