5ba1103df194fe1c62b0e868c68131f7fb201e97
Ben Hoskings <ben@hoskings.net>
2011-06-10 10:11:45 +1000

    Updated git.installer to 1.7.5.4.

--- deps/git.rb
+++ deps/git.rb
@@ -18,7 +18,7 @@ end
 
 dep 'git.installer' do
   requires_when_unmet 'usr-local.install_path'
-  source "http://git-osx-installer.googlecode.com/files/git-1.7.5.3-x86_64-leopard.dmg"
+  source "http://git-osx-installer.googlecode.com/files/git-1.7.5.4-x86_64-leopard.dmg"
   after {
     cd '/usr/local/bin' do
       sudo "ln -sf /usr/local/git/bin/git* ."
