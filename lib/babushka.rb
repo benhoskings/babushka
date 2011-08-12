fa3193c5ec48982682ed7e3a264adc5c7e7dfc73
Ben Hoskings <ben@hoskings.net>
2011-08-12 10:18:30 +1000

    v0.9.2 - Better support for unknown Linux flavours, doc & launch cleanups, --no-colour by default for non-terminal output, fixed bug reporting.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.9.1'
+  VERSION        = '0.9.2'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
