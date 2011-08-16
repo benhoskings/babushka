067ec2cf1d714965ab59b3357ee3aa247b1adfc2
Ben Hoskings <ben@hoskings.net>
2011-08-16 14:48:38 +1000

    Added a yum package list to 'build tools'.

--- deps/dev.rb
+++ deps/dev.rb
@@ -2,6 +2,7 @@ dep 'build tools' do
   requires {
     on :osx, 'xcode tools'
     on :snow_leopard, 'llvm in path'
+    on :yum, dep('gcc'), dep('gcc-c++'), dep('autoconf.managed'), dep('automake.managed'), dep('libtool.managed')
     on :linux, 'build-essential', dep('autoconf.managed'), dep('automake.managed'), dep('libtool.managed')
   }
 end
