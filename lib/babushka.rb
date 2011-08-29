abb7e454cfb0a9aaf0458d4014c2a945736b99d0
Ben Hoskings <ben@hoskings.net>
2011-08-29 22:39:16 +1000

    v0.9.5.1 - A quick semi-revert on top of 0.9.5. See ff20a5c.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.9.5'
+  VERSION        = '0.9.5.1'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
