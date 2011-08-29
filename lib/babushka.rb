dbd3eeee8a77d7305bc3eda9fe61ea25377951a5
Ben Hoskings <ben@hoskings.net>
2011-08-29 11:26:49 +1000

    v0.9.4 - Simpler, less invasive installs (not that they were all that bad before), Fancypath#hypothetically_writable?, a small fix for rubinius, less shelling in install_path deps. `git log v0.9.3..v0.9.4` for details.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.9.3'
+  VERSION        = '0.9.4'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
