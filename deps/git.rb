3e41f54858714809926083902e3d27ab7906f4e4
Ben Hoskings <ben@hoskings.net>
2011-09-05 10:55:38 +1000

    Use git v1.7.6.1 on OS X.

--- deps/git.rb
+++ deps/git.rb
@@ -18,7 +18,7 @@ end
 
 dep 'git.installer' do
   requires_when_unmet 'usr-local.install_path'
-  source "http://git-osx-installer.googlecode.com/files/git-1.7.5.4-x86_64-leopard.dmg"
+  source "http://git-osx-installer.googlecode.com/files/git-1.7.6.1-x86_64-snow-leopard.dmg"
   after {
     cd '/usr/local/bin' do
       sudo "ln -sf /usr/local/git/bin/git* ."
