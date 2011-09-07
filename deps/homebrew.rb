b471c5345d403a4f41c1f907befb1ce2b70181cf
Ben Hoskings <ben@hoskings.net>
2011-09-07 19:12:45 +1000

    Use methods and a GitRepo instead of vars and paths in the homebrew deps.

--- deps/homebrew.rb
+++ deps/homebrew.rb
@@ -1,11 +1,17 @@
 meta :homebrew do
+  def path
+    Babushka::BrewHelper.present? ? Babushka::BrewHelper.prefix : '/usr/local'
+  end
+  def repo
+    Babushka::GitRepo.new path
+  end
 end
 
 dep 'binary.homebrew' do
   requires 'repo.homebrew'
   met? { which 'brew' }
   meet {
-    cd var(:homebrew_prefix) do
+    cd path do
       log_shell "Resetting to HEAD", "git reset --hard"
     end
   }
@@ -13,16 +19,10 @@ end
 
 dep 'repo.homebrew' do
   requires_when_unmet Dep('writable.fhs').with(path), 'git'
-  define_var :homebrew_prefix, :default => '/usr/local', :message => "Where would you like homebrew installed"
-  setup {
-    if Babushka::BrewHelper.present?
-      set :homebrew_prefix, Babushka::BrewHelper.prefix # Use the existing homebrew install if there is one
-    end
-  }
-  met? { File.exists? var(:homebrew_prefix) / '.git' }
+  met? { repo.exists? }
   meet {
-    git "git://github.com/mxcl/homebrew.git" do |path|
-      log_shell "Gitifying #{var :homebrew_prefix}", "cp -r .git '#{var :homebrew_prefix}'"
+    git "git://github.com/mxcl/homebrew.git" do
+      log_shell "Gitifying #{path}", "cp -r .git '#{path}'"
     end
   }
 end
