949fdbafb27f3e0fef09c76fd5d61e9d7422e492
Ben Hoskings <ben@hoskings.net>
2011-09-12 16:44:43 +1000

    Define Babushka::Open3 as a class instead of a module.

--- lib/babushka/popen.rb
+++ lib/babushka/popen.rb
@@ -1,6 +1,6 @@
 module Babushka
-  module Open3
-    def popen3 cmd, opts = {}, &block
+  class Open3
+    def self.popen3 cmd, opts = {}, &block
       pipes = { :in => IO::pipe, :out => IO::pipe, :err => IO::pipe }
       near = { :in => pipes[:in][1], :out => pipes[:out][0], :err => pipes[:err][0] }
       far = { :in => pipes[:in][0], :out => pipes[:out][1], :err => pipes[:err][1] }
@@ -23,16 +23,14 @@ module Babushka
       end
     end
 
-    private
+  private
 
-    def reopen_pipe_for task, pipe, io
+    def self.reopen_pipe_for task, pipe, io
       to_close = pipe[task == :read ? 1 : 0]
       to_reopen = pipe[task == :read ? 0 : 1]
       to_close.close
       io.reopen to_reopen
       to_reopen.close
     end
-
-    module_function :popen3, :reopen_pipe_for
   end
 end
