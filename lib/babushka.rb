f4c30c4ac6f1b152ddb8e3876ac058662e7455dd
Ben Hoskings <ben@hoskings.net>
2011-09-13 11:58:38 +1000

    v0.9.8 - Some tweaks to Babushka::Source, and some commandline specs, both rebased from the distant past. `git log --oneline --graph v0.9.7..v0.9.8` for details.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.9.7'
+  VERSION        = '0.9.8'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
