3421fed683c01b48ae988aa3d47b7c0790c20e8b
Ben Hoskings <ben@hoskings.net>
2011-09-16 11:44:50 +1000

    Install the Xcode commandline tools to satisfy 'build tools' on OS X, since it's much smaller and can be automated.

--- deps/os_x.rb
+++ deps/os_x.rb
@@ -5,6 +5,23 @@ dep 'xcode tools', :template => 'external' do
   }
 end
 
+dep 'xcode commandline tools', :template => 'installer' do
+  # These pkgs consist of the packages inside the Xcode installer that contain the
+  # commandline tools and compiler toolchain. They're not a custom build - they're
+  # unmodified from the original Xcode install; just a subset (i.e. excluding the
+  # packages for things like Interface Builder and Xcode.app).
+  #
+  # See http://github.com/kennethreitz/osx-gcc-installer for more info.
+  source {
+    on :lion,         'https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.7-v2.pkg'
+    on :snow_leopard, 'https://github.com/downloads/kennethreitz/osx-gcc-installer/GCC-10.6.pkg'
+  }
+  provides %w[cc gcc c++ g++ llvm-gcc llvm-g++ clang] # compilers
+  provides %w[ld libtool] # linkety link
+  provides %w[make automake autoconf] # configure and build tools
+  provides %w[cpp m4 nasm yacc bison] # misc - the preprocessor, assembler, grammar stuff
+end
+
 dep 'llvm in path', :for => :snow_leopard do
   requires 'xcode tools'
   met? { which 'llvm-gcc-4.2' }
