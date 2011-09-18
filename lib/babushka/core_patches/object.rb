7af35ae5cdb98957a7183bf0d41bff7611055387
Ben Hoskings <ben@hoskings.net>
2011-09-18 21:07:04 +1000

    We don't need to use Object#instance_exec anymore, so remove the patch.

--- lib/babushka/core_patches/object.rb
+++ lib/babushka/core_patches/object.rb
@@ -25,31 +25,3 @@ class Object
     tap { puts "#{File.basename caller[2]}: #{self.inspect}" }
   end
 end
-
-unless Object.new.respond_to? :instance_exec
-  class Object
-    # Based on http://eigenclass.org/hiki/bounded+space+instance_exec
-    #
-    # But, this version runs against the object's metaclass, instead of
-    # against a shared module on Object, so that methods defined within
-    # the #instance_exec block aren't mistakenly defined on all instances
-    # of the class.
-    #
-    # This is a fallback implementation for older rubies that don't have
-    # a built-in #instance_exec.
-    def instance_exec(*args, &block)
-      begin
-        old_critical, Thread.critical = Thread.critical, true
-        n = 0
-        n += 1 while respond_to?(method_name = "__instance_exec_#{n}")
-        metaclass.send :define_method, method_name, &block
-      ensure
-        Thread.critical = old_critical
-      end
-
-      send method_name, *args
-    ensure
-      metaclass.send :remove_method, method_name
-    end
-  end
-end
