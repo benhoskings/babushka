d4a4278096cb543293c1920d3fde19ed06d6cc34
Ben Hoskings <ben@hoskings.net>
2011-09-20 20:40:23 +1000

    Redesigned the installation deps to use dep parameters instead of vars.

--- deps/babushka.rb
+++ deps/babushka.rb
@@ -1,56 +1,36 @@
-dep 'babushka' do
-  requires 'set up.babushka'
-  setup {
-    set :babushka_branch, 'master'
-    set :install_path, Babushka::Path.path if Babushka::Path.run_from_path?
-  }
-end
-
-dep 'babushka next' do
-  requires 'set up.babushka'
-  setup {
-    set :babushka_branch, 'next'
-    set :install_path, Babushka::Path.path if Babushka::Path.run_from_path?
-  }
-end
-
 meta :babushka do
   def repo
-    Babushka::GitRepo.new var(:install_path)
+    Babushka::GitRepo.new(path)
   end
 end
 
-dep 'set up.babushka' do
-  requires 'up to date.babushka', 'in path.babushka'
-  define_var :install_path, :default => '/usr/local/babushka', :message => "Where would you like babushka installed"
-  define_var :babushka_branch,
-    :message => "Which branch would you like to update from?",
-    :default => 'master',
-    :choice_descriptions => {
-      'master' => 'Standard-issue babushka',
-      'next' => 'The development head -- slight risk of explosions'
-    }
+dep 'babushka', :path, :branch do
+  requires 'up to date.babushka'.with(path, branch)
+  requires 'in path.babushka'.with(path)
+  path.ask("Where would you like babushka installed").default('/usr/local/babushka')
+  branch.default!('master')
 end
 
-dep 'up to date.babushka' do
-  requires 'repo clean.babushka', 'update would fast forward.babushka'
+dep 'up to date.babushka', :path, :branch do
+  requires 'repo clean.babushka'.with(path)
+  requires 'update would fast forward.babushka'.with(path, branch)
   met? {
     (!repo.behind?).tap {|result|
       if result
         log_ok "babushka is up to date at revision #{repo.current_head}."
       else
-        log "babushka can be updated: #{repo.current_head}..#{repo.repo_shell("git rev-parse --short origin/#{var(:babushka_branch)}")}"
+        log "babushka can be updated: #{repo.current_head}..#{repo.repo_shell("git rev-parse --short origin/#{branch}")}"
       end
     }
   }
   meet {
-    log "#{repo.repo_shell("git diff --stat #{repo.current_head}..origin/#{var(:babushka_branch)}")}"
-    repo.reset_hard! "origin/#{var(:babushka_branch)}"
+    log "#{repo.repo_shell("git diff --stat #{repo.current_head}..origin/#{branch}")}"
+    repo.reset_hard! "origin/#{branch}"
   }
 end
 
-dep 'update would fast forward.babushka' do
-  requires 'on correct branch.babushka'
+dep 'update would fast forward.babushka', :path, :branch do
+  requires 'on correct branch.babushka'.with(path, branch)
   met? {
     if !repo.repo_shell('git fetch origin')
       unmeetable "Couldn't pull the latest code - check your internet connection."
@@ -66,27 +46,27 @@ dep 'update would fast forward.babushka' do
   }
 end
 
-dep 'on correct branch.babushka' do
-  requires 'branch exists.babushka'
+dep 'on correct branch.babushka', :path, :branch do
+  requires 'branch exists.babushka'.with(path, branch)
   requires_when_unmet 'repo clean.babushka'
-  met? { repo.current_branch == var(:babushka_branch) }
-  meet { repo.checkout! var(:babushka_branch) }
+  met? { repo.current_branch == branch.to_s }
+  meet { repo.checkout! branch }
 end
 
-dep 'branch exists.babushka' do
+dep 'branch exists.babushka', :path, :branch do
   requires 'installed.babushka'
-  met? { repo.branches.include? var(:babushka_branch) }
-  meet { repo.track! "origin/#{var(:babushka_branch)}" }
+  met? { repo.branches.include? branch.to_s }
+  meet { repo.track! "origin/#{branch}" }
 end
 
-dep 'repo clean.babushka' do
-  requires 'installed.babushka'
+dep 'repo clean.babushka', :path do
+  requires 'installed.babushka'.with(path)
   met? {
     repo.clean? or unmeetable("There are local changes in #{repo.path}.")
   }
 end
 
-dep 'in path.babushka' do
+dep 'in path.babushka', :path do
   requires 'installed.babushka'
   def bin_path
     repo.path / '../bin'
@@ -104,7 +84,7 @@ dep 'in path.babushka' do
   }
 end
 
-dep 'installed.babushka' do
+dep 'installed.babushka', :path do
   requires 'ruby', 'git'
   def babushka_source
     "git://github.com/benhoskings/babushka.git"
