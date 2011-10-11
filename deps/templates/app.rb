87c0edef5f7b528b86ec1613e82d688d0ab03334
Alan Harper <alan@aussiegeek.net>
2011-10-11 13:52:03 +1100

    Install mac app bundles via sparkle

--- deps/templates/app.rb
+++ deps/templates/app.rb
@@ -1,3 +1,6 @@
+require 'rexml/document'
+require 'net/http'
+
 meta :app do
   accepts_list_for :source
   accepts_list_for :prefix, %w[~/Applications /Applications]
@@ -5,6 +8,7 @@ meta :app do
   accepts_list_for :provides, :name
   accepts_block_for :current_version do |path| nil end
   accepts_block_for :latest_version
+  accepts_list_for :sparkle
 
   def app_name_match
     provides.first.to_s.sub(/\.app$/, '*.app')
@@ -34,8 +38,25 @@ meta :app do
     set_version latest_value unless latest_value == true
   end
 
+  def get_source_from_sparkle
+    sparkle_url = sparkle.first
+    puts 'Fetching via sparkle at ' + sparkle_url
+    url = URI.parse(sparkle_url)
+    req = Net::HTTP::Get.new(url.path)
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    doc = REXML::Document.new res.body
+    [doc.elements['rss/channel/item/enclosure'].attributes['url']]
+  end
+
   template {
     prepare {
+      unless sparkle.empty?
+        def source
+          get_source_from_sparkle
+        end
+      end
       setup_source_uris
     }
 
@@ -49,7 +70,7 @@ meta :app do
 
     meet {
       process_sources {|archive|
-        Dir.glob("**/#{app_name_match}").select {|entry|
+        matches = Dir.glob("**/#{app_name_match}").select {|entry|
           (entry / 'Contents/MacOS').exists? # must be an app bundle itself
         }.reject {|entry|
           entry['.app/'] # mustn't be inside another app bundle
