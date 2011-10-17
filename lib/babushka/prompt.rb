d49ce3c94016311d0a1d5a497b47d8d9ff01091f
Ben Hoskings <ben@hoskings.net>
2011-10-17 16:14:17 +1100

    Don't fail when we're not on a terminal but are in defaults mode (and a default is present).

--- lib/babushka/prompt.rb
+++ lib/babushka/prompt.rb
@@ -100,7 +100,7 @@ module Babushka
     def prompt_and_read_value message, opts, &block
       if !opts[:default] && Base.task.opt(:defaults)
         raise DefaultUnavailable.new(message)
-      elsif !$stdin.tty?
+      elsif !Base.task.opt(:defaults) && !$stdin.tty?
         raise PromptUnavailable.new(message)
       else
         log_choice_descriptions opts[:choice_descriptions]
