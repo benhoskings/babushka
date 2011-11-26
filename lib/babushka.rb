2eadc01ea01fae3a4f92797a4194a273b395807e
Ben Hoskings <ben@hoskings.net>
2011-11-26 21:37:37 +1100

    v0.10.9 - Fixed an issue where deps would return previous results when re-run with different arguments. Dep names containing unicode characters work on ruby1.8 now, and more; see below. `git log --oneline v0.10.8..v0.10.9` for details.
    - Dep names that contain unicode characters used to fail on ruby1.8, saying dep
      names can't contain nonprintable characters (which they can't). The issue is
      that the dep names are checked with regexps, and by default, ruby1.8 regexps
      don't consider multibyte characters. The regexp involved is modified with '/mu'
      now so it considers multibyte characters.
    
      The purpose of this is so I can keep my '☕' dep.
    
    - The results returned from a `babushka search`, which queries the public
      database, used to only consider auto-cloneable URLs as such when the scheme was
      git://. Now it ignores the scheme, so https:// (and anything else) is
      considered auto-cloneable.
    
    - When the 'provides' items of a dep exist in more than one place, babushka would
      previously bail out without attempting to run the dep. This is because it
      usually indicates breakage on the system that needs a separate fix. However,
      there are situations where it's fine - e.g. a partial rbenv/rvm ruby install is
      half-shadowing the system ruby. In that case, running the dep will correct it,
      and so now babushka will ask if you'd like to run anyway instead of just
      bailing out.
    
    - The cached result of a dep is a function of its arguments (as well as of the
      system's state), just like a method. But, deps were just caching their result
      once and sticking with it, without considering arguments (it was a holdover
      from before I built dep parameters). This has been fixed now.
    
      Also, deps used to be responsible for caching their own value, which was bad
      design. Now it's done in one place, by Task#cached, which is a simple key/value
      store with a little DSL on it.
    
      Lots of internal refactoring happened around Dep and DepDefiner to support this
      new dep caching. It's also completely lazy now (it was most of the way there,
      it just got a lot cleaner with this refactor).
    
    - Continuing the refactoring theme, the way deps are defined was cleaned up
      significantly too. The deps outer block is run against a DepContext, but the
      logic determining when and how this happens was stored in Dep. It's been moved
      now, and Dep just invokes tasks on the DepContext; DepContext is responsible
      for defining the dep's block as required.
    
      Dep caching uncovered a bug here - the DepContext has to be trashed whenever
      new arguments are passed in, because the dep's outer block has to run for the
      parameter options like param.default() that appear in it to apply to the passed
      parameters. This has been fixed too (it's done in Dep#with, at the point the
      arguments are supplied).

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.10.8'
+  VERSION        = '0.10.9'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
