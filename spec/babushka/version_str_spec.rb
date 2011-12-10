6f1c9d244d0085cdc52de4449885180de87b87bd
Ben Hoskings <ben@hoskings.net>
2011-12-10 11:29:56 +1100

    Rearrange VersionStr specs so parsing is first.

--- spec/babushka/version_str_spec.rb
+++ spec/babushka/version_str_spec.rb
@@ -1,56 +1,6 @@
 require 'spec_helper'
 require 'version_str_support'
 
-def compare_with operator
-  pairs.zip(results[operator]).each {|pair,expected|
-    result = VersionStr.new(pair.first).send operator, VersionStr.new(pair.last)
-    it "#{pair.first} #{operator} #{pair.last}: #{result}" do
-      result.should == expected
-    end
-  }
-end
-
-%w[== != > < >= <= ~>].each do |operator|
-  describe operator do
-    compare_with operator
-  end
-end
-
-describe "comparing" do
-  it "should work with other VersionStrs" do
-    (VersionStr.new('0.3.1') > VersionStr.new('0.2.9')).should be_true
-  end
-  
-  it "should work with strings" do
-    (VersionStr.new('0.3.1') > '0.2.9').should be_true
-  end
-  
-  it "should treat word pieces as less than no piece" do
-    (VersionStr.new('3.0.0') > VersionStr.new('3.0.0.beta')).should be_true
-    (VersionStr.new('3.0.0') > VersionStr.new('3.0.0.beta1')).should be_true
-    (VersionStr.new('1.0.0') > VersionStr.new('1.0.0.rc.5')).should be_true
-  end
-
-  it "should compare word pieces alphabetically" do
-    (VersionStr.new('3.0.0.beta') < VersionStr.new('3.0.0.pre')).should be_true
-    (VersionStr.new('3.0.0.pre') < VersionStr.new('3.0.0.rc')).should be_true
-  end
-
-  it "should treat word pieces with a number as more than without one" do
-    (VersionStr.new('3.0.0.beta1') > VersionStr.new('3.0.0.beta')).should be_true
-  end
-
-  it "should compare number parts of word pieces numerically" do
-    (VersionStr.new('3.0.0.beta2') > VersionStr.new('3.0.0.beta1')).should be_true
-    (VersionStr.new('3.0.0.beta10') > VersionStr.new('3.0.0.beta1')).should be_true
-  end
-  
-  it "should allow for integers in strings and sort correctly" do
-    (VersionStr.new('3.0.0.beta12') > VersionStr.new('3.0.0.beta2')).should be_true
-    (VersionStr.new('R13B04') > VersionStr.new('R2B9')).should be_true
-  end
-end
-
 describe "parsing" do
   it "should parse the version number" do
     VersionStr.new('0.2').pieces.should == [0, 2]
@@ -126,5 +76,55 @@ describe 'rendering' do
   it "should preserve the original formatting" do
     VersionStr.new('1.8.7-p174-src').to_s.should == '1.8.7-p174-src'
     VersionStr.new('3.0.0-beta').to_s.should == '3.0.0-beta'
+  end
+end
+
+def compare_with operator
+  pairs.zip(results[operator]).each {|pair,expected|
+    result = VersionStr.new(pair.first).send operator, VersionStr.new(pair.last)
+    it "#{pair.first} #{operator} #{pair.last}: #{result}" do
+      result.should == expected
+    end
+  }
+end
+
+%w[== != > < >= <= ~>].each do |operator|
+  describe operator do
+    compare_with operator
+  end
+end
+
+describe "comparing" do
+  it "should work with other VersionStrs" do
+    (VersionStr.new('0.3.1') > VersionStr.new('0.2.9')).should be_true
+  end
+
+  it "should work with strings" do
+    (VersionStr.new('0.3.1') > '0.2.9').should be_true
+  end
+
+  it "should treat word pieces as less than no piece" do
+    (VersionStr.new('3.0.0') > VersionStr.new('3.0.0.beta')).should be_true
+    (VersionStr.new('3.0.0') > VersionStr.new('3.0.0.beta1')).should be_true
+    (VersionStr.new('1.0.0') > VersionStr.new('1.0.0.rc.5')).should be_true
+  end
+
+  it "should compare word pieces alphabetically" do
+    (VersionStr.new('3.0.0.beta') < VersionStr.new('3.0.0.pre')).should be_true
+    (VersionStr.new('3.0.0.pre') < VersionStr.new('3.0.0.rc')).should be_true
+  end
+
+  it "should treat word pieces with a number as more than without one" do
+    (VersionStr.new('3.0.0.beta1') > VersionStr.new('3.0.0.beta')).should be_true
+  end
+
+  it "should compare number parts of word pieces numerically" do
+    (VersionStr.new('3.0.0.beta2') > VersionStr.new('3.0.0.beta1')).should be_true
+    (VersionStr.new('3.0.0.beta10') > VersionStr.new('3.0.0.beta1')).should be_true
+  end
+
+  it "should allow for integers in strings and sort correctly" do
+    (VersionStr.new('3.0.0.beta12') > VersionStr.new('3.0.0.beta2')).should be_true
+    (VersionStr.new('R13B04') > VersionStr.new('R2B9')).should be_true
   end
 end
