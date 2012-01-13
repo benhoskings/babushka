5b84292d6893b4f4ba31494a0c4117135e9b084f
Ben Hoskings <ben@hoskings.net>
2012-01-13 12:59:56 +1100

    v0.10.11 - Cleanups to the codebase, some more old method deprecations, and a fix from @madlep for .app version handling when the name has a space. `git log v0.10.10..v0.10.11` for details.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.10.10'
+  VERSION        = '0.10.11'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
