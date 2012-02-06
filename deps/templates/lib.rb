1216dd8d54ea3bb291181222bbc7eafb876fc838
Ben Hoskings <ben@hoskings.net>
2012-02-07 00:35:47 +1100

    Add new 'lib' template, to check for and install library packages via the package manager.

---
+++ deps/templates/lib.rb
@@ -0,0 +1,10 @@
+meta :lib do
+  accepts_list_for :installs, :basename, :choose_with => :via
+
+  template {
+    requires Babushka.host.pkg_helper.manager_dep
+
+    met? { installs.all? {|pkg| Babushka.host.pkg_helper.has?(pkg) } }
+    meet { Babushka.host.pkg_helper.handle_install! installs }
+  }
+end
