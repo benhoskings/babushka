823c8de2d653db296f0855732d31dcb3b84ac691
Ben Hoskings <ben@hoskings.net>
2012-02-06 19:51:48 +1100

    Refactor PathHelper specs to not use ivars.

--- spec/babushka/path_helpers_spec.rb
+++ spec/babushka/path_helpers_spec.rb
@@ -1,20 +1,13 @@
 require 'spec_helper'
 
 describe "cd" do
-  before do
-    @tmp_dir = tmp_prefix
-    `mkdir -p '#{@tmp_dir}'`
-    @tmp_dir_2 = File.join(tmp_prefix, '2')
-    `mkdir -p '#{@tmp_dir_2}'`
-
-    @original_pwd = Dir.pwd
-  end
+  let!(:original_pwd) { Dir.pwd }
 
   it "should yield if no dir is given" do
     has_yielded = false
     cd(nil) {|path|
       path.should be_an_instance_of(Fancypath)
-      Dir.pwd.should == @original_pwd
+      Dir.pwd.should == original_pwd
       has_yielded = true
     }
     has_yielded.should be_true
@@ -22,31 +15,34 @@ describe "cd" do
 
   it "should yield if no chdir is required" do
     has_yielded = false
-    cd(@original_pwd) {|path|
+    cd(original_pwd) {|path|
       path.should be_an_instance_of(Fancypath)
-      Dir.pwd.should == @original_pwd
+      Dir.pwd.should == original_pwd
       has_yielded = true
     }
     has_yielded.should be_true
   end
   it "should change dir for the duration of the block" do
     has_yielded = false
-    cd(@tmp_dir) {
-      Dir.pwd.should == @tmp_dir
+    cd(tmp_prefix) {
+      Dir.pwd.should == tmp_prefix
       has_yielded = true
     }
     has_yielded.should be_true
-    Dir.pwd.should == @original_pwd
+    Dir.pwd.should == original_pwd
   end
-  it "should work recursively" do
-    cd(@tmp_dir) {
-      Dir.pwd.should == @tmp_dir
-      cd(@tmp_dir_2) {
-        Dir.pwd.should == @tmp_dir_2
+  context "recursively" do
+    let(:tmp_subdir) { (tmp_prefix / '2').tap(&:mkdir) }
+    it "should work" do
+      cd(tmp_prefix) {
+        Dir.pwd.should == tmp_prefix
+        cd(tmp_subdir) {
+          Dir.pwd.should == tmp_subdir
+        }
+        Dir.pwd.should == tmp_prefix
       }
-      Dir.pwd.should == @tmp_dir
-    }
-    Dir.pwd.should == @original_pwd
+      Dir.pwd.should == original_pwd
+    end
   end
   context "nonexistent dirs" do
     let(:nonexistent_dir) {
@@ -59,7 +55,7 @@ describe "cd" do
       cd(nonexistent_dir, :create => true) {
         Dir.pwd.should == nonexistent_dir
       }
-      Dir.pwd.should == @original_pwd
+      Dir.pwd.should == original_pwd
     end
     after {
       nonexistent_dir.rm
@@ -68,37 +64,35 @@ describe "cd" do
 end
 
 describe "in_build_dir" do
-  before {
-    @original_pwd = Dir.pwd
-  }
+  let!(:original_pwd) { Dir.pwd }
+
   it "should change to the build dir with no args" do
     in_build_dir {
       Dir.pwd.should == "~/.babushka/build".p
     }
-    Dir.pwd.should == @original_pwd
+    Dir.pwd.should == original_pwd
   end
   it "should append the supplied path when supplied" do
     in_build_dir "tmp" do
       Dir.pwd.should == "~/.babushka/build/tmp".p
     end
-    Dir.pwd.should == @original_pwd
+    Dir.pwd.should == original_pwd
   end
 end
 
 describe "in_download_dir" do
-  before {
-    @original_pwd = Dir.pwd
-  }
+  let!(:original_pwd) { Dir.pwd }
+
   it "should change to the download dir with no args" do
     in_download_dir {
       Dir.pwd.should == "~/.babushka/downloads".p
     }
-    Dir.pwd.should == @original_pwd
+    Dir.pwd.should == original_pwd
   end
   it "should append the supplied path when supplied" do
     in_download_dir "tmp" do
       Dir.pwd.should == "~/.babushka/downloads/tmp".p
     end
-    Dir.pwd.should == @original_pwd
+    Dir.pwd.should == original_pwd
   end
 end
