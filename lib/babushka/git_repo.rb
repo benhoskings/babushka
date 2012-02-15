376fb99f4aa7f9e476a1db52c4a3128fa25858b1
Ben Hoskings <ben@hoskings.net>
2012-02-16 00:33:18 +1100

    Remove raw_path from GitRepo - that #path laziness shouldn't be required.

--- lib/babushka/git_repo.rb
+++ lib/babushka/git_repo.rb
@@ -7,17 +7,15 @@ module Babushka
     include ShellHelpers
     extend ShellHelpers
 
+    attr_reader :path
+
     def self.repo_for path
       maybe = shell("git rev-parse --git-dir", :cd => path) if path.p.dir?
       maybe == '.git' ? path.p : maybe / '..' unless maybe.nil?
     end
 
     def initialize path
-      @raw_path = path
-    end
-
-    def path
-      @path ||= @raw_path.p
+      @path = path.p
     end
 
     def root
