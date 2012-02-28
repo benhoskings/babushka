403535c615ae2e10281c58b85890be316c35361a
Ben Hoskings <ben@hoskings.net>
2012-02-28 12:06:14 +1100

    Convert :ppa meta to 'ppa' parameterised dep, updating the regexp.

--- deps/apt.rb
+++ deps/apt.rb
@@ -23,3 +23,24 @@ dep 'apt source', :uri, :release, :repo do
     Babushka::AptHelper.update_pkg_lists "Updating apt lists to load #{uri}."
   }
 end
+
+dep 'ppa', :spec do
+  requires 'python-software-properties.managed'
+  def present_in_file? filename
+    # e.g. deb http://ppa.launchpad.net/pitti/postgresql/ubuntu natty main
+    filename.p.read[/^deb https?:\/\/.*\/#{spec.gsub(/^.*:/, '')}\/#{Babushka.host.flavour}/]
+  end
+  before {
+    spec[/^\w+\:\w+/] or log_error("'#{spec}' doesn't look like 'ppa:something'.")
+  }
+  met? {
+    present_in_file?('/etc/apt/sources.list') or
+      Dir.glob("/etc/apt/sources.list.d/*").any? {|f| present_in_file?(f) }
+  }
+  meet {
+    sudo "add-apt-repository #{spec}"
+  }
+  after {
+    Babushka::AptHelper.update_pkg_lists "Updating apt lists to load #{spec}."
+  }
+end
