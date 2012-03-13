0133535ea019788e89991a909a7f3e4841600014
Ben Hoskings <ben@hoskings.net>
2012-03-13 23:51:34 +1100

    Reorganise the Renderable specs a bit.

--- spec/babushka/renderable_spec.rb
+++ spec/babushka/renderable_spec.rb
@@ -40,18 +40,18 @@ shared_examples_for 'renderable' do
 end
 
 describe Renderable do
+  subject { Renderable.new(dest_file) }
+
   context "with a config file" do
     let(:source_file) { "spec/renderable/example.conf.erb" }
     let(:dest_file) { tmp_prefix / 'example.conf' }
     let(:content) { %r{root #{tmp_prefix};} }
-    subject { Renderable.new(dest_file) }
     it_should_behave_like 'renderable'
   end
   context "with an xml file" do
     let(:source_file) { "spec/renderable/xml_example.conf.erb" }
     let(:dest_file) { tmp_prefix / 'xml_example.conf' }
     let(:content) { %r{<key>Lol</key>} }
-    subject { Renderable.new(dest_file) }
     it_should_behave_like 'renderable'
     context "custom comment" do
       before { subject.render(source_file, :comment => '<!--', :comment_suffix => '-->') }
@@ -65,36 +65,36 @@ describe Renderable do
     let(:source_file) { "spec/renderable/example.sh" }
     let(:dest_file) { tmp_prefix / 'example.sh' }
     let(:content) { %r{babushka 'benhoskings:up to date.repo'} }
-    subject { Renderable.new(dest_file) }
     it_should_behave_like 'renderable'
   end
-  describe "binding handling" do
-    subject { Renderable.new(tmp_prefix / 'example.conf') }
-    context "when no explicit binding is passed" do
-      before {
-        subject.instance_eval {
-          def custom_renderable_path
-            "from implicit binding"
-          end
-        }
-        subject.render('spec/renderable/with_binding.conf.erb')
+end
+
+describe "binding handling" do
+  subject { Renderable.new(tmp_prefix / 'example.conf') }
+  context "when no explicit binding is passed" do
+    before {
+      subject.instance_eval {
+        def custom_renderable_path
+          "from implicit binding"
+        end
       }
-      it "should render using the implicit binding" do
-        (tmp_prefix / 'example.conf').read.should =~ /from implicit binding/
-      end
+      subject.render('spec/renderable/with_binding.conf.erb')
+    }
+    it "should render using the implicit binding" do
+      (tmp_prefix / 'example.conf').read.should =~ /from implicit binding/
     end
-    context "when an explicit binding is passed" do
-      before {
-        dep 'renderable binding spec' do
-          def custom_renderable_path
-            "from explicit binding"
-          end
-        end.met?
-        subject.render('spec/renderable/with_binding.conf.erb', :context => Dep('renderable binding spec').context)
-      }
-      it "should render using the given binding" do
-        (tmp_prefix / 'example.conf').read.should =~ /from explicit binding/
-      end
+  end
+  context "when an explicit binding is passed" do
+    before {
+      dep 'renderable binding spec' do
+        def custom_renderable_path
+          "from explicit binding"
+        end
+      end.met?
+      subject.render('spec/renderable/with_binding.conf.erb', :context => Dep('renderable binding spec').context)
+    }
+    it "should render using the given binding" do
+      (tmp_prefix / 'example.conf').read.should =~ /from explicit binding/
     end
   end
 end
