2f4f9e7e88b7bb32062bcb4dd715ceb73928416c
Ben Hoskings <ben@hoskings.net>
2012-03-20 11:22:22 +1100

    v0.12.3 - A quick update with a new nodejs.src, better #append_to_file deprecation, and the copyright year bumped (thanks to @bearded for noticing it was out of date).

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.12.2'
+  VERSION        = '0.12.3'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
