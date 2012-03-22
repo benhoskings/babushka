24b9e9abe2642dda871b2c618056bc78bff01943
Ben Hoskings <ben@hoskings.net>
2012-03-22 12:29:16 +1100

    v0.12.4 - Some patches, thanks to @bearded: git-1.7.9.4, deprecation fixes, and doc updates.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.12.3'
+  VERSION        = '0.12.4'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
