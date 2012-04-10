e43adf7a29de73e96a398f759f2c87911f7bd62f
Ben Hoskings <ben@hoskings.net>
2012-04-10 17:41:34 +1000

    Reorganise Fancypath's methods into groups.

--- lib/fancypath/fancypath.rb
+++ lib/fancypath/fancypath.rb
@@ -15,8 +15,6 @@ class Fancypath < Pathname
     alias :p :to_expanded_fancypath
   end
 
-  # methods are chainable and do what you think they do
-
   alias_method :to_str, :to_s unless method_defined? :to_str
 
   alias_method :dir, :dirname
@@ -31,6 +29,17 @@ class Fancypath < Pathname
   alias_method :exists?, :exist?
   alias_method :rename_to, :rename
 
+
+  # About this Fancypath object.
+
+  def inspect
+    super.sub('Pathname','Fancypath')
+  end
+
+  def to_fancypath
+    self
+  end
+
   def == other
     if other.is_a? String
       to_s == other
@@ -43,144 +52,8 @@ class Fancypath < Pathname
     to_s.length
   end
 
-  def join(path)
-    path_str = path.to_s
-    super(path_str[0..0] == '/' ? path_str[1..-1] : path_str).p
-  end
 
-  alias_method :/, :join
-
-  # This method returns true if the path is writable (i.e. and already
-  # exists), or if the current user would be able to create it (i.e. if
-  # its closest existing parent is writable).
-  def hypothetically_writable?
-    writable? || (!exists? && !root? && parent.hypothetically_writable?)
-  end
-
-  # make file
-  def touch
-    `touch '#{self}'`
-    self
-  end
-
-  def create_dir
-    mkpath unless exist?
-    self
-  end
-
-  alias_method :create, :create_dir
-
-  def copy(dest)
-    `cp -pPR '#{self}' '#{dest}'`
-    self
-  end
-
-  alias_method :cp, :copy
-
-  # file or dir
-  def remove
-    directory? ? rmtree : delete if exist?
-    self
-  end
-  alias_method :rm, :remove
-
-  def read
-    super.chomp if exists?
-  end
-
-  def grep *args
-    if exists?
-      matches = read.split("\n").grep(*args)
-      matches unless matches.empty?
-    end
-  end
-
-  def yaml
-    require 'yaml'
-    YAML.load_file self
-  end
-
-  def readlink
-    if !symlink?
-      self
-    elsif
-      target = super
-      target.absolute? ? target : (dir / target)
-    end
-  end
-
-  def mkdir
-    `mkdir -p '#{self}'`
-    self
-  end
-
-  def glob expr = nil, flags = File::FNM_CASEFOLD, &block
-    Dir.glob((expr.nil? ? self : (self / expr)).to_s, flags, &block)
-  end
-
-  def write(contents, mode='wb')
-    dirname.create
-    open(mode) { |f| f.write contents }
-    self
-  end
-
-  def append(contents)
-    write(contents,'a+')
-    self
-  end
-
-  def move(dest)
-    self.rename(dest)
-    dest.p
-  end
-
-  def tail(bytes)
-    return self.read if self.size < bytes
-    open('r') do |f|
-      f.seek(-bytes, IO::SEEK_END)
-      f.read
-    end
-  end
-
-  alias_method :mv, :move
-
-  def set_extension(ext)
-    "#{without_extension}.#{ext}".p
-  end
-
-  alias_method :change_extension, :set_extension
-
-  def without_extension
-    to_s[/^ (.+?) (\. ([^\.]+))? $/x, 1].p
-  end
-
-  def has_extension?(ext)
-    !!(self.to_s =~ /\.#{ext}$/)
-  end
-
-  def parent
-    super.p
-  end
-
-  alias_method :all_children, :children
-
-  def children
-    super.reject { |c| c.basename.to_s =~ /^\./ }
-  end
-
-  # only takes sym atm
-  def select(*args)
-    return args.map { |arg| select(arg) }.flatten.uniq if args.size > 1
-
-    case arg = args.first
-    when Symbol
-      Dir["#{self}/*.#{arg}"].map { |p| self.class.new(p) }
-    when Regexp
-      children.select { |child| child.to_s =~ arg }
-    else
-      Dir["#{self}/#{arg}"].map { |p| self.class.new(p) }
-    end
-  end
+  # Querying the path.
 
   def empty?
     directory? ? children.size == 0 : self.size == 0
@@ -194,11 +67,151 @@ class Fancypath < Pathname
     Etc.getgrgid(File.stat(to_s).gid).name
   end
 
-  def inspect
-    super.sub('Pathname','Fancypath')
+  # This method returns true if the path is writable (i.e. and already
+  # exists), or if the current user would be able to create it (i.e. if
+  # its closest existing parent is writable).
+  def hypothetically_writable?
+    writable? || (!exists? && !root? && parent.hypothetically_writable?)
   end
 
-  def to_fancypath
+
+  # Path traversal & manipulation.
+
+  def join(path)
+    path_str = path.to_s
+    super(path_str[0..0] == '/' ? path_str[1..-1] : path_str).p
+  end
+  alias_method :/, :join
+
+  def parent
+    super.p
+  end
+
+  def children
+    super.reject { |c| c.basename.to_s =~ /^\./ }
+  end
+  alias_method :all_children, :children
+
+  def readlink
+    if !symlink?
+      self
+    elsif
+      target = super
+      target.absolute? ? target : (dir / target)
+    end
+  end
+
+
+  # Querying the file the path refers to.
+
+  def read
+    super.chomp if exists?
+  end
+
+  def tail(bytes)
+    return self.read if self.size < bytes
+    open('r') do |f|
+      f.seek(-bytes, IO::SEEK_END)
+      f.read
+    end
+  end
+
+  def yaml
+    require 'yaml'
+    YAML.load_file self
+  end
+
+
+  # Querying the tree below the dir the path refers to.
+
+  def grep *args
+    if exists?
+      matches = read.split("\n").grep(*args)
+      matches unless matches.empty?
+    end
+  end
+
+  def glob expr = nil, flags = File::FNM_CASEFOLD, &block
+    Dir.glob((expr.nil? ? self : (self / expr)).to_s, flags, &block)
+  end
+
+  def select(*args)
+    return args.map { |arg| select(arg) }.flatten.uniq if args.size > 1
+
+    case arg = args.first
+    when Symbol
+      Dir["#{self}/*.#{arg}"].map { |p| self.class.new(p) }
+    when Regexp
+      children.select { |child| child.to_s =~ arg }
+    else
+      Dir["#{self}/#{arg}"].map { |p| self.class.new(p) }
+    end
+  end
+
+
+  # Filename manupulation.
+
+  def set_extension(ext)
+    "#{without_extension}.#{ext}".p
+  end
+  alias_method :change_extension, :set_extension
+
+  def without_extension
+    to_s[/^ (.+?) (\. ([^\.]+))? $/x, 1].p
+  end
+
+  def has_extension?(ext)
+    !!(self.to_s =~ /\.#{ext}$/)
+  end
+
+
+  # Changing the file or dir the path refers to.
+
+  def touch
+    `touch '#{self}'`
+    self
+  end
+
+  def mkdir
+    `mkdir -p '#{self}'`
+    self
+  end
+
+  def create_dir
+    mkpath unless exist?
+    self
+  end
+  alias_method :create, :create_dir
+
+  def copy(dest)
+    `cp -pPR '#{self}' '#{dest}'`
+    self
+  end
+  alias_method :cp, :copy
+
+  def move(dest)
+    self.rename(dest)
+    dest.p
+  end
+  alias_method :mv, :move
+
+  def remove
+    directory? ? rmtree : delete if exist?
+    self
+  end
+  alias_method :rm, :remove
+
+
+  # Changing the contents of the file the path refers to.
+
+  def write(contents, mode='wb')
+    dirname.create
+    open(mode) { |f| f.write contents }
+    self
+  end
+
+  def append(contents)
+    write(contents,'a+')
     self
   end
 
