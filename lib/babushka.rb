c2154b1304c34ed4c9a34170263a406a92154ddd
Ben Hoskings <ben@hoskings.net>
2012-04-24 15:47:46 +1000

    v0.12.6 - A quick fix for some outdated #log_warn calls that were exposed by v0.12.5.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.12.5'
+  VERSION        = '0.12.6'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
