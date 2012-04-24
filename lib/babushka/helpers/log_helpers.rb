benhoskings/babushka

commit 1c388ff84ac3814f787614873ea9a966ee670e57

Use module_function in LogHelpers to make the methods directly callable, and private when included.

lib/babushka/helpers/log_helpers.rb
@@ -2,6 +2,11 @@
 
 module Babushka
   module LogHelpers
+
+    # Make these helpers callable directly on LogHelpers,
+    # and private when included.
+    module_function
+
     # Log +message+ as an error. This is a shortcut for
     #   log(message, :as => :error)
     def log_error message, opts = {}, &block

commit 384918945dc324b52221be191344d43e96c6ecf8

Remove the unused (and broken since 1c388ff) Babushka::Log.

lib/babushka/helpers/log_helpers.rb
@@ -179,8 +179,4 @@ module Babushka
       @indentation_level -= 1
     end
   end
-
-  class Log
-    extend LogHelpers
-  end
 end