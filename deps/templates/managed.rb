d5c439069caaab9d745a1a833d359ff31800899f
Ben Hoskings <ben@hoskings.net>
2012-05-13 19:06:38 +1000

    Remove config generation logic from 'managed' templates -- it's not used in any babushka dep on github, except one old one of mine. It's also untested.

--- deps/templates/managed.rb
+++ deps/templates/managed.rb
@@ -7,28 +7,7 @@ managed_template = L{
     packages.all? {|pkg| pkg_manager.has? pkg }
   end
 
-  def add_cfg_deps
-    cfg.all? {|target|
-      target_file = target.to_s
-      source_file = load_path.dirname / name / "#{File.basename(target_file)}.erb"
-      requires(dep("#{File.basename(target_file)} for #{name}") {
-        met? { babushka_config? target_file }
-        before {
-          shell "mkdir -p #{File.dirname(target_file)}", :sudo => !File.writable?(File.dirname(File.dirname(target_file)))
-          shell "chmod o+rx #{File.dirname(target_file)}", :sudo => !File.writable?(File.dirname(target_file))
-        }
-        meet { render_erb source_file, :to => target_file, :sudo => !File.writable?(File.dirname(target_file)) }
-        on :linux do
-          after { service_name.each {|s| sudo "/etc/init.d/#{s} restart" } }
-        end
-      })
-    }
-  end
-
   requires pkg_manager.manager_dep
-  prepare {
-    add_cfg_deps
-  }
   met? {
     if installs.blank?
       log_ok "Nothing to install on #{pkg_manager.manager_key}-based systems."
@@ -47,8 +26,6 @@ managed_template = L{
 meta :managed do
   accepts_list_for :installs, :basename, :choose_with => :via
   accepts_list_for :provides, :basename, :choose_with => :via
-  accepts_list_for :service_name, :name
-  accepts_list_for :cfg
 
   def pkg_manager
     Babushka.host.pkg_helper
@@ -60,8 +37,6 @@ end
 meta :gem do
   accepts_list_for :installs, :basename, :choose_with => :via
   accepts_list_for :provides, :basename, :choose_with => :via
-  accepts_list_for :service_name, :name
-  accepts_list_for :cfg
 
   def pkg_manager
     Babushka::GemHelper
@@ -73,8 +48,6 @@ end
 meta :pip do
   accepts_list_for :installs, :basename, :choose_with => :via
   accepts_list_for :provides, :basename, :choose_with => :via
-  accepts_list_for :service_name, :name
-  accepts_list_for :cfg
 
   def pkg_manager
     Babushka::PipHelper
@@ -86,8 +59,6 @@ end
 meta :npm do
   accepts_list_for :installs, :basename, :choose_with => :via
   accepts_list_for :provides, [], :choose_with => :via
-  accepts_list_for :service_name, :name
-  accepts_list_for :cfg
 
   def pkg_manager
     Babushka::NpmHelper
