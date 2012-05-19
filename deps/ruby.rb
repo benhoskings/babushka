0596dd3554e6d062ef6e73c2fe64e965bd527cf2
Ben Hoskings <ben@hoskings.net>
2012-05-19 20:00:56 +1000

    No need for a separate 'ruby' dep using #requires_when_unmet now we're using the 'bin' template.

--- deps/ruby.rb
+++ deps/ruby.rb
@@ -1,16 +1,9 @@
-dep 'ruby' do
-  met? {
-    in_path? ['ruby >= 1.8.6', 'irb']
-  }
-  requires_when_unmet 'ruby.bin'
-end
-
-dep 'ruby.bin' do
+dep 'ruby', :template => 'bin' do
   installs {
     via [:lenny, :hardy, :lucid], %w[ruby irb ruby1.8-dev libopenssl-ruby]
     via :apt, %w[ruby ruby1.8-dev]
     via :yum, %w[ruby ruby-irb]
     otherwise 'ruby'
   }
-  provides %w[ruby irb]
+  provides 'ruby >= 1.8.6', 'irb'
 end
