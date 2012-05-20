7e457a13f4720f5fda0bad6148536ae28c18a8f3
Ben Hoskings <ben@hoskings.net>
2012-05-20 18:35:47 +1000

    Remove 'llvm in path' -- it's not required for any recent versions.

--- deps/dev.rb
+++ deps/dev.rb
@@ -1,7 +1,6 @@
 dep 'build tools' do
   requires {
     on :osx, 'xcode commandline tools'
-    on :snow_leopard, 'llvm in path'
     on :yum, dep('gcc'), dep('gcc-c++'), dep('autoconf.bin'), dep('automake.bin'), dep('libtool.bin')
     on :linux, 'build-essential', dep('autoconf.bin'), dep('automake.bin'), dep('libtool.bin')
   }
--- deps/os_x.rb
+++ deps/os_x.rb
@@ -21,13 +21,3 @@ dep 'xcode commandline tools', :template => 'installer' do
   provides %w[make] # configure and build tools
   provides %w[cpp m4 nasm yacc bison] # misc - the preprocessor, assembler, grammar stuff
 end
-
-dep 'llvm in path', :for => :snow_leopard do
-  requires 'xcode tools'
-  met? { which 'llvm-gcc-4.2' }
-  meet {
-    cd('/usr/local/bin') {|path|
-      shell "ln -s /Developer/usr/llvm-gcc-4.2/bin/llvm* .", :sudo => !path.writable?
-    }
-  }
-end
