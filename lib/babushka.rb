ef2c2261c28b2e55dee4acaaa64249451e8f3063
Ben Hoskings <ben@hoskings.net>
2012-05-22 00:48:10 +1000

    v0.12.8 - babushka will trigger cron emails when deps fail now.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.12.7'
+  VERSION        = '0.12.8'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
