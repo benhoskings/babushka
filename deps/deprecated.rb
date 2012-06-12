4b87cc87a90375c337d4f54a9a76159a412ac3d0
Ben Hoskings <ben@hoskings.net>
2012-06-12 19:16:52 +1000

    Refactor dep deprecations to allow any pair of old/new names to be mapped, not just basenames to their new templates.

--- deps/deprecated.rb
+++ deps/deprecated.rb
@@ -1,15 +1,15 @@
 {
-  'curl'    => 'bin',
-  'gettext' => 'lib',
-  'git'     => 'bin',
-  'pip'     => 'bin',
-  'ruby'    => 'bin',
-  'sudo'    => 'bin'
-}.each_pair {|basename, new_template|
+  'curl.managed'    => 'curl.bin',
+  'gettext.managed' => 'gettext.lib',
+  'git.managed'     => 'git.bin',
+  'pip.managed'     => 'pip.bin',
+  'ruby.managed'    => 'ruby.bin',
+  'sudo.managed'    => 'sudo.bin'
+}.each_pair {|old_name, new_name|
 
-  dep "#{basename}.managed" do
-    deprecated! "2012-11-14", :method_name => "'#{name}'", :callpoint => false, :instead => "the '#{basename}.#{new_template}' dep"
-    requires "#{basename}.#{new_template}"
+  dep old_name do
+    deprecated! "2012-11-14", :method_name => "'#{name}'", :callpoint => false, :instead => "the '#{new_name}' dep"
+    requires new_name
   end
 
 }
