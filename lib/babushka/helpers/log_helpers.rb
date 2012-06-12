a2bc7a8a25d59a3f8717f7ff650e452122a2fb50
Ben Hoskings <ben@hoskings.net>
2012-06-12 20:26:15 +1000

    Add a :skip option to #deprecated!, to skip down the backtrace if required.

--- lib/babushka/helpers/log_helpers.rb
+++ lib/babushka/helpers/log_helpers.rb
@@ -57,7 +57,7 @@ module Babushka
     end
 
     def deprecated! date, opts = {}
-      callpoint = "#{caller[1].sub(/\:in `.*$/, '')}: " unless opts[:callpoint] == false
+      callpoint = "#{caller[opts[:skip] || 1].sub(/\:in `.*$/, '')}: " unless opts[:callpoint] == false
       opts[:method_name] ||= "##{caller[0].scan(/`(\w+)'$/).flatten.first}"
       log_warn "#{callpoint}#{opts[:method_name]} has been deprecated and will be removed on #{date}."
       log_warn "  -> Use #{opts[:instead]} instead#{opts[:example] ? ", e.g. #{opts[:example]}" : '.'}" unless opts[:instead].nil?
