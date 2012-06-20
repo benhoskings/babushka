59e2bdcb5e8028c43edbfcb3cea24b3d7eff8187
Ben Hoskings <ben@hoskings.net>
2012-06-20 16:58:22 +1000

    On install, just checkout the supplied ref, instead of creating & updating a corresponding local branch.
    closes gh-203

--- deps/babushka.rb
+++ deps/babushka.rb
@@ -14,56 +14,27 @@ end
 
 dep 'up to date.babushka', :from, :path, :branch do
   requires 'repo clean.babushka'.with(from, path)
-  requires 'update would fast forward.babushka'.with(from, path, branch)
-  met? {
-    (!repo.behind?).tap {|result|
-      if result
-        log_ok "babushka is up to date at #{repo.current_head}."
-      else
-        log "babushka can be updated: #{repo.current_head}..#{repo.repo_shell("git rev-parse --short origin/#{branch}")}"
-      end
-    }
-  }
-  meet {
-    log "#{repo.repo_shell("git diff --stat #{repo.current_head}..origin/#{branch}")}"
-    repo.reset_hard! "origin/#{branch}"
-  }
-end
-
-dep 'update would fast forward.babushka', :from, :path, :branch do
-  requires 'on correct branch.babushka'.with(from, path, branch)
+  def refspec
+    qualified_branch = branch['/'].nil? ? "origin/#{branch}" : branch
+    repo.resolve(qualified_branch)
+  end
   met? {
     if !repo.repo_shell('git fetch origin')
       unmeetable! "Couldn't pull the latest code - check your internet connection."
     else
-      if !repo.remote_branch_exists?
-        unmeetable! "The current branch, #{repo.current_branch}, isn't pushed to origin/#{repo.current_branch}."
-      elsif repo.ahead?
-        unmeetable! "There are unpushed commits in #{repo.current_branch}."
-      else
-        true
-      end
+      (repo.current_head == refspec).tap {|result|
+        if result
+          log_ok "babushka is up to date at #{repo.current_head}."
+        else
+          log "babushka can be updated: #{repo.current_head}..#{refspec}"
+        end
+      }
     end
   }
-end
-
-dep 'on correct branch.babushka', :from, :path, :branch do
-  requires 'branch exists.babushka'.with(from, path, branch)
-  requires_when_unmet 'repo clean.babushka'.with(from, path)
-
-  setup {
-    # Stay on the same branch unless one was specified.
-    repo = Babushka::GitRepo.new(path)
-    branch.default!(repo.current_branch) if repo.exists?
+  meet {
+    log "#{repo.repo_shell("git diff --stat #{repo.current_head}..#{refspec}")}"
+    repo.detach!(refspec)
   }
-  met? { repo.current_branch == branch.to_s }
-  meet { log_block("Switching to #{branch}") { repo.checkout! branch } }
-end
-
-dep 'branch exists.babushka', :from, :path, :branch do
-  requires 'installed.babushka'.with(from, path)
-  met? { repo.branches.include? branch.to_s }
-  meet { log_block("Checking out origin/#{branch}") { repo.track! "origin/#{branch}" } }
 end
 
 dep 'repo clean.babushka', :from, :path do
