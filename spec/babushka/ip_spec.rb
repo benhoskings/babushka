fa6a8c248c8d2f37de2a9b4caab1d8502c1a7177
Ben Hoskings <ben@hoskings.net>
2012-08-12 00:13:42 +1000

    Whitespace.

--- spec/babushka/ip_spec.rb
+++ spec/babushka/ip_spec.rb
@@ -8,8 +8,8 @@ describe "IP" do
   end
   it "should work for valid IPs" do
     [
-      '10.0.1.1'      ,
-      '192.168.0.1'   ,
+      '10.0.1.1',
+      '192.168.0.1',
       '174.129.110.43'
     ].each {|string|
       IP.new(string).should be_valid
@@ -17,10 +17,10 @@ describe "IP" do
   end
   it "should work for invalid IPs" do
     [
-      '10.0.1.'        ,
-      '.10.0.1'        ,
-      '192.168.0'      ,
-      '192.168.0.c'    ,
+      '10.0.1.',
+      '.10.0.1',
+      '192.168.0',
+      '192.168.0.c',
       '174.129.110.433'
     ].each {|string|
       IP.new(string).should_not be_valid
@@ -54,8 +54,8 @@ end
 describe "IPRange" do
   it "should work for valid IPs" do
     [
-      '10.0.1.x'   ,
-      '10.0.x'     ,
+      '10.0.1.x',
+      '10.0.x',
       '174.129.x.x'
     ].each {|string|
       IPRange.new(string).should be_valid
@@ -63,9 +63,9 @@ describe "IPRange" do
   end
   it "should work for invalid IPs" do
     [
-      '10.0.1.1'    ,
-      '10.0.x.1'    ,
-      '10.0.x.'     ,
+      '10.0.1.1',
+      '10.0.x.1',
+      '10.0.x.',
       '174.129.110.',
       '174.129.x.43'
     ].each {|string|
