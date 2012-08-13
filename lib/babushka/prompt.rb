041feb91d3c00fc21454ae61e7833d37a111f329
Ben Hoskings <ben@hoskings.net>
2012-08-13 16:00:15 +1000

    Brackets.

--- lib/babushka/prompt.rb
+++ lib/babushka/prompt.rb
@@ -23,7 +23,7 @@ module Babushka
 
   class << self
     def suggest_value_for typo, choices
-      if (possible_matches = choices.similar_to typo.to_s).empty?
+      if (possible_matches = choices.similar_to(typo.to_s)).empty?
         nil # nothing to suggest
       elsif possible_matches.length == 1
         confirm "#{"Did you mean".colorize('grey')} '#{possible_matches.first}'#{"?".colorize('grey')}" do
