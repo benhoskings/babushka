5eb2137488a68afefd5b1e3284e801fc6bc503f1
Ben Hoskings <ben@hoskings.net>
2012-08-13 16:01:36 +1000

    v0.13.5 - Undefined deps don't cause a prompt anymore.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.13.4'
+  VERSION        = '0.13.5'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
