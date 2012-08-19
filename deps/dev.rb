6c9df9a1c758e9185ce3044409ec0a6eaaaff3b7
Peter Gumeson <gumeson@gmail.com>
2012-08-18 22:23:35 -0700

    Removed deprecated log_and_open from xcode deps

--- deps/dev.rb
+++ deps/dev.rb
@@ -13,7 +13,11 @@ end
 dep 'xcode tools', :template => 'external' do
   expects 'gcc', 'g++', 'autoconf', 'make', 'ld'
   otherwise {
-    log_and_open "Install Xcode, and then run Babushka again.", "http://developer.apple.com/technology/xcode.html"
+    log "Install Xcode, and then run Babushka again."
+    log "Official download at http://developer.apple.com/technology/xcode.html"
+    confirm "Open in browser now" do
+      shell "open http://developer.apple.com/technology/xcode.html"
+    end
   }
 end
 
@@ -24,7 +28,10 @@ dep 'xcode commandline tools', :template => 'external' do
   expects %w[make] # configure and build tools
   expects %w[cpp m4 nasm yacc bison] # misc - the preprocessor, assembler, grammar stuff
   otherwise {
-    log_and_open "Install Command Line Tools for Xcode, and then run Babushka again.",
-                 "http://developer.apple.com/downloads"
+    log "Install Command Line Tools for Xcode, and then run Babushka again."
+    log "Official pacakge at http://developer.apple.com/downloads"
+    confirm "Open in browser now" do
+      shell "open http://developer.apple.com/downloads"
+    end
   }
 end
