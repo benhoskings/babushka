dd3e0af3aad7d74b6d8dcf7bac9cabda0600053c
Ben Hoskings <ben@hoskings.net>
2012-08-29 10:28:30 +1000

    v0.13.8 - GitRepo#detach! works on older versions of git now.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.13.7'
+  VERSION        = '0.13.8'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
