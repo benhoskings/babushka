0b66f464d1d0d08976b281a7cfbd88469edc6cb7
Ben Hoskings <ben@hoskings.net>
2012-09-10 14:28:55 +1000

    Revert "Bump node to v0.8.8."
    This reverts commit c958b078db6458d6a9a0813ba5f2e34e781f1b02.

--- deps/packages.rb
+++ deps/packages.rb
@@ -3,8 +3,8 @@ dep 'curl.bin'
 dep 'gettext.lib'
 
 dep 'nodejs.src', :version do
-  version.default!('0.8.8')
-  source "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
+  version.default!('0.6.13')
+  source "http://nodejs.org/dist/node-v#{version}.tar.gz"
   provides "node ~> #{version}"
 end
 
