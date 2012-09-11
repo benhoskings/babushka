da320501e74dd0bb8defea2164cfa3e1eff9ed8c
Ben Hoskings <ben@hoskings.net>
2012-09-11 11:29:46 +1000

    v0.14.0 - A lot of previously deprecated methods have been removed:
    * Babushka::Base.host
    * DepDefiner#met, #unmet & #unmeetable
    * most of RunHelpers
    
    Their stubs have been left behind as temporary guides.
    
    Also, node is installed from a package now that ubuntu 12.04 includes a recent version.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.13.8'
+  VERSION        = '0.14.0'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
