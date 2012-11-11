924794a846c4c6163a8dfffe015ad70bb48572cb
Ben Hoskings <ben@hoskings.net>
2012-11-11 18:22:58 +1100

    v0.15.2 - Fixed clean installs of remote refs, like babushka.me/up/next.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.15.1'
+  VERSION        = '0.15.2'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
