0bd1ff86b6e0f8b522a43bbf23fc80e3052a60f7
Ben Hoskings <ben@hoskings.net>
2012-11-11 17:55:53 +1100

    v0.15.1 - A parameter default fix and git dep updates.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.15.0'
+  VERSION        = '0.15.1'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
