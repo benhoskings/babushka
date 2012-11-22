08849cbaeaf506a443696211cbbcea2ea958b5fc
Ben Hoskings <ben@hoskings.net>
2012-11-22 14:26:10 +1100

    v0.15.4 - Note the babushka and ruby versions in the debug log.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.15.3'
+  VERSION        = '0.15.4'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
