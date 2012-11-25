3e4aa249321efcad9cd5d67eeff30103a2ce0bb3
Ben Hoskings <ben@hoskings.net>
2012-11-25 13:05:45 +1100

    Tweak the profiling option description.

--- spec/babushka/cmdline/help_spec.rb
+++ spec/babushka/cmdline/help_spec.rb
@@ -52,7 +52,7 @@ describe "help" do
     -u, --update                     Update referenced sources before loading deps from them
         --show-args                  Show the arguments being passed between deps as they're run
         --track-blocks               Track deps' blocks in TextMate as they're run
-        --profile                    Emit profiling information.
+        --profile                    Print a per-line timestamp to the debug log.
 ")
 
       Cmdline.should_receive(:log).with("\n")
