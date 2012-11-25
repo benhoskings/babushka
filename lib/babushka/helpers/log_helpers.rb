a7812c2fbba07bd4dca13b7f85acfe68c9ce5c94
Ben Hoskings <ben@hoskings.net>
2012-11-25 13:13:48 +1100

    Print profiling info in grey like other admin logging, instead of using a divider.

--- lib/babushka/helpers/log_helpers.rb
+++ lib/babushka/helpers/log_helpers.rb
@@ -106,7 +106,7 @@ module Babushka
       printable = !opts[:debug] || Base.task.opt(:debug)
       if Base.task.opt(:profile)
         delta = Time.now - Base.start_time
-        Logging.print_log("%.4f :: " % delta, printable, opts[:as])
+        Logging.print_log("%.4f ".colorize('grey') % delta, printable, opts[:as])
       end
       Logging.print_log(Logging.indentation, printable, opts[:as]) unless opts[:indentation] == false
       if block_given?
