77cab3e004d52673f030e82071736809e6bc155c
Ben Hoskings <ben@hoskings.net>
2013-01-29 00:13:31 +1100

    Remove vars from bug reports.

--- lib/babushka/bug_reporter.rb
+++ lib/babushka/bug_reporter.rb
@@ -3,15 +3,14 @@ module Babushka
     extend LogHelpers
     extend ShellHelpers
 
-    # This method creates a bug report for +dep+, by reading the debug log and
-    # vars associated with it and posting them as a gist. If the github user is
+    # This method creates a bug report for +dep+, by reading the debug log
+    # associated with it and posting them as a gist. If the github user is
     # set in the git config, it's marked as from that user, otherwise it's
     # anonymous.
     def self.report dep
       Prompt.confirm "I can file a bug report for that now, if you like.", :default => 'n', :otherwise => "OK, you're on your own :)" do
         post_report dep,
           (which('git') && shell('git config github.user')) || 'anonymous',
-          Base.task.var_path_for(dep).read,
           Base.task.log_path_for(dep).read
       end
     end
@@ -20,14 +19,13 @@ module Babushka
     private
 
     # gist.github.com API example at http://gist.github.com/4277
-    def self.post_report dep, user, vars, log
+    def self.post_report dep, user, log
       require 'net/http'
       require 'uri'
 
       Net::HTTP.post_form(
         URI.parse('http://gist.github.com/api/v1/xml/new'), {
           "files[from]" => user,
-          "files[vars.yml]" => vars,
           "files[#{dep.contextual_name}.log]" => log.decolorize
         }
       ).tap {|response|
@@ -45,10 +43,7 @@ module Babushka
           log "You can view the report at http://gist.github.com/#{gist_id} - thanks :)"
         end
       else
-        log_stderr "Deary me, the bug report couldn't be submitted! Would you mind emailing these two files:"
-        log_stderr '  ' + Base.task.var_path_for(dep)
-        log_stderr '  ' + Base.task.log_path_for(dep)
-        log_stderr "to ben@hoskings.net? Thanks."
+        log_stderr "Deary me, the bug report couldn't be submitted! Would you mind emailing the contents of '#{Base.task.log_path_for(dep)}' to ben@hoskings.net? Thanks."
       end
     end
   end
