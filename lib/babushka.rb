f9427530b6402555166f494984c47f422a1fd90b
Ben Hoskings <ben@hoskings.net>
2013-02-01 22:29:48 +1100

    v0.15.7 - Template namespacing, TTYless specs, AWS apt sources, and /location:/i.
    * Templates in the 'personal' and 'current dir' sources are useable without namespacing now.
    * Stub $stdin.tty? to test Prompt, so that the specs run correctly when not on a terminal. (Thanks to Richo Healey).
    * Only enable fuubar when on a terminal. (Thanks to Richo Healey).
    * Don't replace AWS apt mirrors with the standard ones, so EC2 instanaces retain their default, fast mirrors. (Thanks to Lucas Chan.)
    * Search for the Location: header case-insensitively when downloading in Resource, as per the spec; some servers use 'location:'. (Thanks to Brad Feehan.)
    * Create npm's package dir after installing it, so that certain npm operations don't fail when it doesn't exist.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.15.6'
+  VERSION        = '0.15.7'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
