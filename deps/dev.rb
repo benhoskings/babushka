bd561dde5f6efca0f7587da40e822abab961cddf
Ben Hoskings <ben@hoskings.net>
2013-02-03 23:15:49 +1100

    Consolidate xcode tools deps into a single external with an updated 'provides'.

--- deps/dev.rb
+++ deps/dev.rb
@@ -1,6 +1,6 @@
 dep 'build tools' do
   requires {
-    on :osx, 'xcode commandline tools'
+    on :osx, 'xcode tools'
     on :linux, 'linux build tools'
   }
 end
@@ -14,27 +14,8 @@ dep 'linux build tools', :template => 'bin' do
 end
 
 dep 'xcode tools', :template => 'external' do
-  expects 'gcc', 'g++', 'autoconf', 'make', 'ld'
+  expects %w[cc gcc c++ g++ llvm-gcc llvm-g++ clang make ld libtool]
   otherwise {
-    log "Install Xcode, and then run Babushka again."
-    log "Official download at http://developer.apple.com/technology/xcode.html"
-    confirm "Open in browser now" do
-      shell "open http://developer.apple.com/technology/xcode.html"
-    end
-  }
-end
-
-dep 'xcode commandline tools', :template => 'external' do
-  # See http://kennethreitz.com/xcode-gcc-and-homebrew.html
-  expects %w[cc gcc c++ g++ llvm-gcc llvm-g++ clang] # compilers
-  expects %w[ld libtool] # linkety link
-  expects %w[make] # configure and build tools
-  expects %w[cpp m4 nasm yacc bison] # misc - the preprocessor, assembler, grammar stuff
-  otherwise {
-    log "Install Command Line Tools for Xcode, and then run Babushka again."
-    log "Official pacakge at http://developer.apple.com/downloads"
-    confirm "Open in browser now" do
-      shell "open http://developer.apple.com/downloads"
-    end
+    log "Install Xcode via the App Store, then go Preferences -> Downloads -> Components -> Command Line Tools."
   }
 end
