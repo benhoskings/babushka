ac6585eaef81cbe3573ae2417fd40cddb5cc834f
Ben Hoskings <ben@hoskings.net>
2013-02-03 20:39:22 +1100

    Use long options in PacmanHelper.

--- lib/babushka/pkg_helpers/pacman_helper.rb
+++ lib/babushka/pkg_helpers/pacman_helper.rb
@@ -9,7 +9,7 @@ module Babushka
     private
 
     def has_pkg? pkg_name
-      raw_shell("pacman -Q #{pkg_name}").stderr !~ /not found$/
+      raw_shell("pacman --query #{pkg_name}").stderr !~ /not found$/
     end
 
     # NOTE By default, Arch has sudo's `tty_tickets` option enabled. This will
@@ -20,7 +20,7 @@ module Babushka
     # tl;dr - Add the following line to /etc/sudoers:
     #         Defaults !tty_tickets
     def install_pkgs! pkgs, opts
-      log_shell "Downloading", "#{pkg_cmd} -S --noconfirm #{pkgs.join(' ')}", :sudo => should_sudo?
+      log_shell "Downloading", "#{pkg_cmd} --sync --noconfirm #{pkgs.join(' ')}", :sudo => should_sudo?
     end
 
   end
