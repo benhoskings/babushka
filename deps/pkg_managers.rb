benhoskings/babushka

commit 61fc4223380ffe6644d11f2d1beb94223fa27d42

On OS X, install pip via homebrew instead of from source.

deps/pkg_managers.rb
@@ -39,18 +39,11 @@ end
 
 dep 'pip' do
   requires {
-    on :osx, 'pip.src'
+    via :brew, 'python.bin' # homebrew installs pip along with python.
     otherwise 'pip.bin'
   }
 end
 
 dep 'pip.bin' do
   installs 'python-pip'
-end
-
-dep 'pip.src' do
-  source 'http://pypi.python.org/packages/source/p/pip/pip-0.8.3.tar.gz'
-  process_source {
-    log_shell "Installing pip", "python setup.py install", :sudo => !which('python').p.writable?
-  }
 end

deps/python.rb
@@ -1,0 +1,6 @@
+dep 'python.bin' do
+  provides {
+    via :brew, 'python', 'pip' # homebrew installs pip along with python.
+    otherwise 'python'
+  }
+end

commit e209f2a76d394b6957cda55d32413b92ae8a7499

Fix incorrect chooser in 'pip' (introduced in 61fc422).

deps/pkg_managers.rb
@@ -39,7 +39,7 @@ end
 
 dep 'pip' do
   requires {
-    via :brew, 'python.bin' # homebrew installs pip along with python.
+    on :brew, 'python.bin' # homebrew installs pip along with python.
     otherwise 'pip.bin'
   }
 end