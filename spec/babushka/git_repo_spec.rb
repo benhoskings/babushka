e7dac360423310fa296745598c0790c65f2a3d8b
richo <richo@psych0tik.net>
2013-02-28 13:23:23 +1100

    Update spec for #resolve

--- spec/babushka/git_repo_spec.rb
+++ spec/babushka/git_repo_spec.rb
@@ -301,7 +301,7 @@ describe GitRepo, '#resolve' do
   before { stub_repo 'a' }
   subject { Babushka::GitRepo.new(tmp_prefix / 'repos/a') }
   it "should return a full commit id" do
-    subject.resolve('master').should =~ /^[0-9a-f]{7,40}$/
+    subject.resolve('master').should =~ /^[0-9a-f]{40}$/
   end
 end
 
