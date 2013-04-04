9b923f201add96e4e6d023cc00d4311e3b6ccbab
Ben Hoskings <ben@hoskings.net>
2013-04-04 21:45:52 +1100

    Add new dep counter helpers that use expectations instead of the yield counter hack.

--- spec/babushka/dep_support.rb
+++ spec/babushka/dep_support.rb
@@ -11,6 +11,26 @@ def setup_yield_counts
   @yield_counts_failed_at_before = {:setup => 1, :met? => 2, :prepare => 1, :before => 1}
 end
 
+def should_call_dep_like type, dep
+  expectations = {
+    :none                => {:setup => 0, :met? => 0, :prepare => 0, :before => 0, :meet => 0, :after => 0},
+    :met_run             => {:setup => 1, :met? => 1, :prepare => 0, :before => 0, :meet => 0, :after => 0},
+    :meet_run            => {:setup => 1, :met? => 2, :prepare => 1, :before => 1, :meet => 1, :after => 1},
+    :dep_failed          => {:setup => 1, :met? => 0, :prepare => 0, :before => 0, :meet => 0, :after => 0},
+    :failed_meet_run     => {:setup => 1, :met? => 2, :prepare => 1, :before => 1, :meet => 1, :after => 1},
+    :early_exit_meet_run => {:setup => 1, :met? => 1, :prepare => 1, :before => 1, :meet => 1, :after => 0},
+    :already_met         => {:setup => 1, :met? => 1, :prepare => 0, :before => 0, :meet => 0, :after => 0},
+    :failed_at_before    => {:setup => 1, :met? => 2, :prepare => 1, :before => 1, :meet => 0, :after => 0},
+  }[type]
+  expect_dep_blocks(dep, expectations)
+end
+
+def expect_dep_blocks receiver, expectations
+  expectations.each_pair {|method_name, times|
+    receiver.should_receive(:process_task).with(method_name).exactly(times).times.and_call_original
+  }
+end
+
 def make_counter_dep opts = {}
   incrementers = DepContext.accepted_blocks.inject({}) {|lambdas,key|
     lambdas[key] = L{ @yield_counts[opts[:name]][key] += 1 }
