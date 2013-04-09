366ded98b7775d0bb15444dad6f64069b31a6757
Ben Hoskings <ben@hoskings.net>
2013-04-09 19:33:22 +1000

    v0.16.7 - Much clearer met?/meet logic in Babushka::Dep, to reflect the babushka concept directly.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.16.6'
+  VERSION        = '0.16.7'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
