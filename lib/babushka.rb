4ff73e0eda5ff439fd786e4a3bea8568abc95fe2
Ben Hoskings <ben@hoskings.net>
2013-06-12 16:07:14 +1000

    v0.16.10 - Added a 'common:dep' source, refactored some modules away, and removed some old, unused classes.

--- lib/babushka.rb
+++ lib/babushka.rb
@@ -1,5 +1,5 @@
 module Babushka
-  VERSION        = '0.16.9'
+  VERSION        = '0.16.10'
   WorkingPrefix  = '~/.babushka'
   SourcePrefix   = '~/.babushka/sources'
   BuildPrefix    = '~/.babushka/build'
