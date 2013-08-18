34a717fcc28684b9d691a39668ac71bc314d5ed6
Ben Hoskings <ben@hoskings.net>
2013-08-18 17:18:00 +1000

    Remove duplicate logging when adding sources on the commandline.

--- lib/babushka/source.rb
+++ lib/babushka/source.rb
@@ -135,9 +135,7 @@ module Babushka
         log "Nothing to add for #{name}."
       else
         raise_unless_addable!
-        log_block "Adding #{name} from #{uri}" do
-          update!
-        end
+        update!
       end
     end
 
