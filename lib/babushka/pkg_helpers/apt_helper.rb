b55241df7c7d9966a8fac592ab880b67d05c3807
Laurent Vallar <val@zbla.net>
2013-10-09 09:37:33 +0200

    Add Debian geoip mirror support

--- lib/babushka/pkg_helpers/apt_helper.rb
+++ lib/babushka/pkg_helpers/apt_helper.rb
@@ -31,7 +31,7 @@ module Babushka
 
     def source_matcher_for_system
       {
-        :debian => %r{http://ftp\d?.(\w\w\.)?debian.org/debian/?},
+        :debian => %r{http://(ftp\d?.(\w\w\.)?debian.org|(http|cdn).debian.net)/debian/?},
         :ubuntu => %r{http://((\w\w-(.*)-\d\.ec2\.)|(\w\w\.))?archive.ubuntu.com/ubuntu/?}
       }[Babushka.host.flavour]
     end
