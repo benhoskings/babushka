02fbc8fb0053211dd167b9f0f90d01b5434ace3b
Ben Hoskings <ben@hoskings.net>
2014-06-09 02:22:12 +0900

    Fix references to package manager name in logging.
    This should never have been #manager_dep; that's a dep for use at
    runtime, and not intended to be a readable name on its own.
    
    Using #manager_key isn't perfect, but it's the name by which the
    package manager is referenced in the deps, so it's the right value.

--- lib/babushka/pkg_helper.rb
+++ lib/babushka/pkg_helper.rb
@@ -67,15 +67,15 @@ module Babushka
       else
         list_age = Time.now - pkg_list_dir.mtime
         if list_age > pkg_update_timeout
-          update_pkg_lists "#{manager_dep.capitalize} package lists are #{list_age.round.xsecs} old. Updating"
+          update_pkg_lists "The #{manager_key} package lists are #{list_age.round.xsecs} old. Updating"
         else
-          debug "#{manager_dep.capitalize} package lists are #{list_age.round.xsecs} old (up to date)."
+          debug "The #{manager_key} package lists are #{list_age.round.xsecs} old (up to date)."
           true # up to date
         end
       end
     end
 
-    def update_pkg_lists message = "Updating #{manager_dep.capitalize} package lists"
+    def update_pkg_lists message = "Updating #{manager_key} package lists"
       log_shell message, pkg_update_command, :sudo => should_sudo?
     end
 
