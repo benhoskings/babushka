f8fc27aedc1b01ac861288d9a2cf651b3f2488cc
Ben Hoskings <ben@hoskings.net>
2014-07-28 21:57:37 +0900

    Make the 'multiple installations' log message a bit clearer.
    Answering 'y' at this prompt says "yes, attempt to meet the dep", not
    "yes, consider it met".
    
    That is, the choice is between attempting to continue as normal and
    bailing out immediately, not whether or not to bypass this dep's
    normal logic.

--- lib/babushka/path_checker.rb
+++ lib/babushka/path_checker.rb
@@ -20,7 +20,7 @@ module Babushka
         log_error dir_hash.values.map {|cmds|
             cmd_location_str_for cmds
           }.to_list(:oxford => true, :conj => 'but').end_with('.')
-        unmeetable! unless Prompt.confirm("Multiple installations might indicate a problem. Meet anyway?", :default => 'n')
+        unmeetable! unless Prompt.confirm("Multiple installations might indicate a problem. Attempt to meet the dep anyway?", :default => 'n')
       else
         dir_hash[nil].blank?.tap {|result|
           if result
