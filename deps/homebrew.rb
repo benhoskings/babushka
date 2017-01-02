a0ace806352551610cb672bff4374a3927906c32
Tim Riley <tim@openmonkey.com>
2017-01-03 09:41:26 +1100

    Use standard Homebrew installation script instead of manual git checkout
    Homebrew is split across 2 repos now, and their install script takes care of getting them both setup as required.

--- deps/homebrew.rb
+++ deps/homebrew.rb
@@ -8,19 +8,12 @@ meta :homebrew do
 end
 
 dep 'binary.homebrew' do
-  requires 'repo.homebrew'
-  met? { in_path? 'brew' }
-  meet { log_shell "Resetting to HEAD", "git reset --hard", :cd => path }
-end
-
-dep 'repo.homebrew' do
-  requires_when_unmet 'writable.fhs'.with(path), 'git'
   met? {
-    repo.exists?
+    in_path? 'brew'
   }
   meet {
-    git "https://github.com/mxcl/homebrew.git" do
-      log_shell "Gitifying #{path}", "cp -r .git '#{path}'"
-    end
+    log_shell \
+      'Installing Homebrew',
+      'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
   }
 end
