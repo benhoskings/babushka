538734af5682bc285ded30ccde20dbcafc4be775
Ben Hoskings <ben@hoskings.net>
2017-03-03 18:42:00 +1100

    Deprecate Hashish.

--- lib/babushka/core_patches/hashish.rb
+++ lib/babushka/core_patches/hashish.rb
@@ -2,6 +2,7 @@ class Hashish
   # Return a new hash whose default value is an empty array; i.e. querying any
   # un-assigned key will assign and return an empty array to that key.
   def self.array
+    Babushka::LogHelpers.deprecated! '2017-09-01', instead: "`Hash.new {|hsh,k| hsh[k] = [] }`"
     Hash.new {|hsh,k| hsh[k] = [] }
   end
 
@@ -9,6 +10,7 @@ class Hashish
   # un-assigned key will assign and return an empty hash to that key. Note that
   # the empty hash is a regular hash and doesn't have any default keys of its own.
   def self.hash
+    Babushka::LogHelpers.deprecated! '2017-09-01', instead: "`Hash.new {|hsh,k| hsh[k] = {} }`"
     Hash.new {|hsh,k| hsh[k] = {} }
   end
 end
