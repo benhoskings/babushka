78f0c325248ce2dd5ac6a67d628375224afb0eee
Ben Hoskings <ben@hoskings.net>
2017-03-03 19:07:58 +1100

    Remove Object#tap definition.
    This has been in ruby core as of 1.8.7, so we don't need to add it
    anymore.

--- lib/babushka/core_patches/object.rb
+++ lib/babushka/core_patches/object.rb
@@ -8,18 +8,6 @@ class Object
     class << self; self end
   end
 
-  # Return this object after yielding it to the block. This is useful for
-  # neatly working with an object in some way before returning it:
-  #   def valmorphanize
-  #     process.tap {|result|
-  #       log_error "Oops!" unless result
-  #     }
-  #   end
-  def tap &block
-    yield self
-    self
-  end unless Object.respond_to?(:tap)
-
   # Return self unmodified after logging the output of #inspect, along with
   # the point at which +tapp+ was called.
   def tapp
